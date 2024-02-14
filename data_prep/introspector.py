4c09bf7c314800f4bc480b0430602e1085803405
Dongge Liu <donggeliu@google.com>
2024-02-14 14:14:29 +1100

    Adopt function signature from FI directly

--- data_prep/introspector.py
+++ data_prep/introspector.py
@@ -33,7 +33,6 @@ TIMEOUT = 10
 INTROSPECTOR_ENDPOINT = 'https://introspector.oss-fuzz.com/api'
 INTROSPECTOR_CFG = f'{INTROSPECTOR_ENDPOINT}/annotated-cfg'
 INTROSPECTOR_FUNCTION = f'{INTROSPECTOR_ENDPOINT}/far-reach-but-low-coverage'
-INTROSPECTOR_SIGNATURE = f'{INTROSPECTOR_ENDPOINT}/function-signature'
 
 
 def query_introspector_for_unreached_functions(project: str) -> list[dict]:
@@ -48,28 +47,6 @@ def query_introspector_for_unreached_functions(project: str) -> list[dict]:
   logging.error('No functions found from FI for project %s:\n  %s', project,
                 '\n  '.join(data.get('extended_msgs')))
   sys.exit(1)
-
-
-def _query_introspector_for_funtion_signature(project_name: str,
-                                              raw_function_name: str) -> str:
-  """
-  Queries function_signature from FuzzIntrospector |project_name| and
-  |raw_function_name|.
-  """
-  resp = requests.get(INTROSPECTOR_SIGNATURE,
-                      params={
-                          'project': project_name,
-                          'function': raw_function_name
-                      },
-                      timeout=TIMEOUT)
-  data = resp.json()
-  function_signature = data.get('signature')
-  if function_signature:
-    return function_signature
-  logging.error(
-      'No function signature found from FuzzIntrospector for project '
-      '(%s) function (%s).', project_name, raw_function_name)
-  return ''
 
 
 def query_introspector_cfg(project):
@@ -132,11 +109,6 @@ def _get_raw_function_name(function: dict) -> str:
           function.get('raw_function_name', ''))
 
 
-def _get_demangled_function_name(function: dict) -> str:
-  """Returns the demangled function name."""
-  return demangle(_get_raw_function_name(function))
-
-
 def _get_clean_arg_types(function: dict) -> list[str]:
   """Returns the cleaned function argument types."""
   raw_arg_types = (function.get('arg-types') or
@@ -150,33 +122,15 @@ def _get_arg_names(function: dict) -> list[str]:
           function.get('function_argument_names', ''))
 
 
-def get_function_signature(project_name: str, function: dict,
-                           language: str) -> str:
-  """
-  Returns function signature of |function| from |project_name|.
-  """
-  # TODO(Dongge): Use FuzzIntrospector by default, when it is ready.
-  # function_signature = _query_introspector_for_funtion_signature(
-  #     project_name, _get_raw_function_name(function))
-  # if function_signature:
-  #   return function_signature
-
-  # C++ functions, get signature from c++filt.
-  if language == benchmarklib.FileType.CPP.value.lower():
-    function_signature = _get_demangled_function_name(function)
-    if function_signature != _get_raw_function_name(function):
-      return function_signature
-
-  # Plain C function or C++ unmangled function.
-  return_type = _get_clean_return_type(function)
-  function_arg_types = _get_clean_arg_types(function)
-  function_arg_names = _get_arg_names(function)
-
-  args_signature = ', '.join([
-      f'{arg_type} {arg_name}'
-      for arg_type, arg_name in zip(function_arg_types, function_arg_names)
-  ])
-  return f'{return_type} {_get_raw_function_name(function)}({args_signature})'
+def get_function_signature(function: dict, project: str) -> str:
+  """Returns the function signature."""
+  function_signature = function.get('function_signature')
+  if function_signature:
+    return function_signature
+  logging.warning(
+      'Missing function signature in project: %s\n  raw_function_name: %s',
+      project, _get_raw_function_name(function))
+  return ''
 
 
 # TODO(dongge): Remove this function when FI fixes it.
@@ -227,8 +181,7 @@ def populate_benchmarks_using_introspector(project: str, language: str,
       # TODO: Bazel messes up paths to include "/proc/self/cwd/..."
       logging.error('error: %s %s', filename, interesting.keys())
       continue
-    # TODO(dongge): Remove this line when FI provides function_signature.
-    function_signature = get_function_signature(project, function, language)
+    function_signature = get_function_signature(function, project)
     if not function_signature:
       continue
     logging.info('Function signature to fuzz: %s', function_signature)
--- data_prep/project_targets.py
+++ data_prep/project_targets.py
@@ -86,7 +86,7 @@ def _match_target_path_content(target_paths: List[str],
 
 def _bucket_match_target_content_signatures(
     target_funcs: Dict[str, List[Dict]], fuzz_target_dir: str,
-    project_language: str, project_name: str) -> Dict[str, List[str]]:
+    project_name: str) -> Dict[str, List[str]]:
   """Returns a list of dictionary with function signatures as keys and
     its fuzz target content as values."""
   if not target_funcs:
@@ -120,8 +120,7 @@ def _bucket_match_target_content_signatures(
       target_content_signature_dict[content] = []
 
     signatures = [
-        introspector.get_function_signature(project_name, func_info,
-                                            project_language)
+        introspector.get_function_signature(func_info, project_name)
         for func_info in functions
     ]
     target_content_signature_dict[content].extend(signatures)
@@ -136,9 +135,8 @@ def generate_data(project_name: str,
   """Generates project-specific fuzz targets examples."""
   target_funcs = introspector.get_project_funcs(project_name)
   project_fuzz_target_dir = _get_fuzz_target_dir(project_name)
-  project_language = oss_fuzz_checkout.get_project_language(project_name)
   target_content_signature_dict = _bucket_match_target_content_signatures(
-      target_funcs, project_fuzz_target_dir, project_language, project_name)
+      target_funcs, project_fuzz_target_dir, project_name)
 
   if target_content_signature_dict:
     print(f'Downloaded human-written fuzz targets of {project_name} from Google'
@@ -148,7 +146,7 @@ def generate_data(project_name: str,
           f'from Google Cloud Bucket: {OSS_FUZZ_EXP_BUCKET}.')
     print('Will try to build from Google Cloud or local docker image.')
     target_content_signature_dict = _match_target_content_signatures(
-        target_funcs, project_name, project_language, cloud_experiment_bucket)
+        target_funcs, project_name, cloud_experiment_bucket)
   if not target_content_signature_dict:
     return []
 
@@ -200,7 +198,6 @@ def filter_target_lines(target_content: str) -> str:
 def _match_target_content_signatures(
     target_funcs: Dict[str, List[Dict]],
     project_name: str,
-    project_language: str,
     cloud_experiment_bucket: str = '') -> Dict[str, List[str]]:
   """Returns a list of dictionary with function signatures as keys and
     its fuzz target content as values."""
@@ -239,8 +236,7 @@ def _match_target_content_signatures(
       target_content_signature_dict[content] = []
 
     signatures = [
-        introspector.get_function_signature(project_name, func_info,
-                                            project_language)
+        introspector.get_function_signature(func_info, project_name)
         for func_info in functions
     ]
     target_content_signature_dict[content].extend(signatures)
