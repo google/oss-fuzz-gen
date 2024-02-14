436b4669a058ec7c593068ea1b94dd966bb3955b
Dongge Liu <donggeliu@google.com>
2024-02-14 11:28:21 +1100

    Use function signature in report (#102)
    The current report displays function by its `raw_function_name`, which
    is not human-readable.
    This PR replaces that with the corresponding function signature found in
    the benchmark set used in the experiment.

--- report/web.py
+++ report/web.py
@@ -23,6 +23,7 @@ import sys
 import urllib.parse
 from typing import List, Optional
 
+import yaml
 from flask import Flask, abort, render_template
 
 import run_one_experiment
@@ -34,15 +35,40 @@ log = logging.getLogger('werkzeug')
 log.setLevel(logging.ERROR)
 
 RESULTS_DIR = ''
+BENCHMARK_SET_DIR = 'benchmark-sets'
+BENCHMARK_DIR = ''
 
 MAX_RUN_LOGS_LEN = 16 * 1024
 
 
 @dataclasses.dataclass
 class Benchmark:
+  """The class of a benchmark function and its experiment results."""
   id: str
   status: str
   result: run_one_experiment.AggregatedResult
+  signature: str = ''
+
+  def __post_init__(self):
+    self.signature = self._find_signature() or self.id
+
+  def _find_signature(self) -> str:
+    """Finds the function signature by searching for its id in BENCHMARK_DIR."""
+    if not BENCHMARK_DIR:
+      return ''
+
+    for project_yaml in os.listdir(BENCHMARK_DIR):
+      yaml_project_name = project_yaml.removesuffix(".yaml")
+      with open(os.path.join(BENCHMARK_DIR, project_yaml)) as project_yaml_file:
+        if yaml_project_name not in self.id:
+          continue
+        functions = yaml.safe_load(project_yaml_file).get('functions', [])
+        for function in functions:
+          function_name = self.id.removeprefix(f'output-{yaml_project_name}-')
+          if function.get('name', '').lower().startswith(function_name):
+            return f'{yaml_project_name}-{function.get("signature", "")}'
+
+    return ''
 
 
 @dataclasses.dataclass
@@ -115,7 +141,7 @@ def _is_valid_benchmark_dir(cur_dir: str) -> bool:
   if not cur_dir.startswith('output-'):
     return False
   # Check sub-directories.
-  expected_dirs = ['raw_targets', 'status', 'fixed_targets', 'logs', 'corpora']
+  expected_dirs = ['raw_targets', 'status', 'fixed_targets']
   return all(
       os.path.isdir(os.path.join(RESULTS_DIR, cur_dir, expected_dir))
       for expected_dir in expected_dirs)
@@ -299,14 +325,16 @@ def cov_report_link(link: str):
   return f'https://llm-exp.oss-fuzz.com/{path}/report/linux/report.html'
 
 
-def serve(directory: str, port: int):
-  global RESULTS_DIR
+def serve(directory: str, port: int, benchmark_set: str):
+  global RESULTS_DIR, BENCHMARK_DIR
   RESULTS_DIR = directory
+  BENCHMARK_DIR = os.path.join(BENCHMARK_SET_DIR, benchmark_set)
   app.run(host='localhost', port=port)
 
 
 if __name__ == '__main__':
   results_dir = sys.argv[1]
   server_port = int(sys.argv[2])
+  benchmark_dir = sys.argv[3] if len(sys.argv) > 2 else ''
 
-  serve(results_dir, server_port)
+  serve(results_dir, server_port, benchmark_dir)
