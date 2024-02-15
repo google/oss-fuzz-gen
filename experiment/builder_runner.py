5f78fc7a5679b03a0a2c5f232692e6ee199db5e6
Jim Choi <cjxian0304@gmail.com>
2024-02-15 03:18:27 +0000

    add reference to errors

--- experiment/builder_runner.py
+++ experiment/builder_runner.py
@@ -325,7 +325,9 @@ class BuilderRunner:
 class CloudBuilderRunner(BuilderRunner):
   """Cloud BuilderRunner."""
   _RETRYABLE_ERRORS = [
+      # Temp workaround for issue #12.
       'You do not currently have an active account selected',
+      # Workaround for issue #85.
       'gcloud crashed (OSError): unexpected end of data'
   ]
 
@@ -374,10 +376,9 @@ class CloudBuilderRunner(BuilderRunner):
                cwd=oss_fuzz_checkout.OSS_FUZZ_DIR)
         break
       except sp.CalledProcessError as e:
-        # Remove \n for better logging on cloud.
+        # Remove \n for single log entry on cloud.
         stdout = e.stdout.decode('utf-8').replace('\n', '\t')
         stderr = e.stderr.decode('utf-8').replace('\n', '\t')
-        # Temp workaround for issue #12.
         if (any(error in stdout + stderr for error in self._RETRYABLE_ERRORS)
             and attempt_id < CLOUD_EXP_MAX_ATTEMPT):
           delay = 5 * 2**attempt_id
