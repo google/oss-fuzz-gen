4298ef938d24d1c4549a620a417c508e385e49c2
Richard Yang <ggryan@google.com>
2024-03-07 20:53:04 +1100

    fix

--- data_prep/project_context/context_introspector.py
+++ data_prep/project_context/context_introspector.py
@@ -1,9 +1,11 @@
 """Class to retrieve context from introspector for
 better prompt generation."""
 
+import os
+
 from data_prep import introspector
 from experiment import benchmark as benchmarklib
-import os
+
 
 class ContextRetriever:
   """Class to retrieve context from introspector for
@@ -31,41 +33,40 @@ class ContextRetriever:
     Simply queries for a single line in source code."""
     file_name = os.path.normpath(info['source']['source_file'])
     typedef_line = info['source']['source_line']
-    source_line = introspector.query_introspector_source_code(self._benchmark.project,
-                                                              file_name,
-                                                              typedef_line,
-                                                              typedef_line)
+    source_line = introspector.query_introspector_source_code(
+        self._benchmark.project, file_name, typedef_line, typedef_line)
     # Check to ensure typedef is within the source line.
     # There are instances where it isn't.
     # One example, is when it is combined with a struct declaration
     # 1. typedef X {
     # 2.   T elem1;
     # 3. } X;
-    # The typedef element has value 3. In this case, it would be better to ignore the string.
+    # The typedef element has value 3. In this case,
+    # it would be better to ignore the string.
     if 'typedef' not in source_line:
       return ''
 
     return source_line
 
-  def _get_struct_type(self, info: dict, seen_types: set, types_to_get: set) -> str:
+  def _get_struct_type(self, info: dict, seen_types: set,
+                       types_to_get: set) -> str:
     """Reconstructs type definition from a struct element.
     Also adds newly seen types to recursively collect definitions."""
     # Attempt to recursively retrieve type information for elements
+    elements = info['elements']
     file_name = os.path.normpath(info['source']['source_file'])
     begin_line = info['source']['source_line']
-    end_line = info['elements'][:-1]['source']['source_line']
+    end_line = elements[:-1]['source']['source_line']
     curr_line = begin_line
 
     reconstructed_type = ''
     # The reason we need to iteratively query FI is because
     # we have to parse each individual source line and
-    # extract a possible type which we will recursively 
+    # extract a possible type which we will recursively
     # query the definition for.
     while curr_line <= end_line:
-      source_line = introspector.query_introspector_source_code(self._benchmark.project,
-                                                                file_name,
-                                                                curr_line,
-                                                                curr_line)
+      source_line = introspector.query_introspector_source_code(
+          self._benchmark.project, file_name, curr_line, curr_line)
       print("Source line: {}".format(source_line))
 
       if not source_line:
@@ -77,13 +78,15 @@ class ContextRetriever:
       if curr_line == begin_line:
         continue
 
-      newly_seen_type = self._clean_type(self._extract_type_from_source_line(source_line, elements[curr_line - begin_line - 1]))
+      newly_seen_type = self._clean_type(
+          self._extract_type_from_source_line(
+              source_line, elements[curr_line - begin_line - 1]))
       if not newly_seen_type or newly_seen_type in seen_types:
         continue
 
       print("Newly seen type: {}".format(newly_seen_type))
       seen_types.add(newly_seen_type)
-      type_to_get.add(newly_seen_type)
+      types_to_get.add(newly_seen_type)
       curr_line += 1
 
   def get_embeddable_types(self) -> list[str]:
@@ -105,7 +108,7 @@ class ContextRetriever:
     seen_types = types_to_get
 
     print("Querying for types: {}".format(types_to_get))
-    
+
     # Add support for recursively querying for types
     while types_to_get:
       current_type = types_to_get.pop()
@@ -116,9 +119,11 @@ class ContextRetriever:
         continue
 
       # For now, info is a single element and not a list.
-      # Requested it be changed to a list, so that type elements with the same name will be returned.
+      # Requested it be changed to a list, so that type 
+      # elements with the same name will be returned.
       if info['type'] == 'struct':
-        reconstructed_type = self._get_struct_type(info, seen_types, types_to_get)
+        reconstructed_type = self._get_struct_type(info, seen_types,
+                                                   types_to_get)
       if info['type'] == 'typedef':
         reconstructed_type = self._get_typedef_type(info)
 
@@ -127,7 +132,8 @@ class ContextRetriever:
 
     return list(types)
 
-  def _extract_type_from_source_line(self, source_line: str, type_element: dict) -> str:
+  def _extract_type_from_source_line(self, source_line: str,
+                                     type_element: dict) -> str:
     """Attempts to extract a type from a source line."""
     # Do so by attempting to match for name and extracting everything before it
 
@@ -175,3 +181,6 @@ class ContextRetriever:
     into the prompt."""
     types = self.get_embeddable_types()
     decl = self.get_embeddable_declaration()
+
+    blob = '\n'.join(types) + decl
+    return blob
