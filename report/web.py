64c542cef724910e20c348601747fb5284762626
Dongge Liu <donggeliu@google.com>
2024-04-02 09:58:44 +1100

    Fix report sample page (#197)
    Fixes incorrect usage of `sample`:
    It was a string on that page, now changing it to the `Sample` Object so
    that we can show its results and crash reason on the page.
    
    Preview:
    <img width="1848" alt="image"
    src="https://github.com/google/oss-fuzz-gen/assets/20501961/e04ea927-0854-4a81-ae9d-852c532c4725">

--- report/web.py
+++ report/web.py
@@ -242,6 +242,25 @@ def get_samples(benchmark: str) -> list[Sample]:
   return samples
 
 
+def match_sample(benchmark: str, target_sample_id: str) -> Optional[Sample]:
+  """Identifies the samples object and its status of the given sample id."""
+  results, _ = get_results(benchmark)
+
+  for i, sample_id in enumerate(sample_ids(get_generated_targets(benchmark))):
+    if sample_id != target_sample_id:
+      continue
+    status = 'Running'
+    result = None
+    if results[i]:
+      status = 'Done'
+      result = results[i]
+
+    return Sample(sample_id, status, result)
+  logging.warning('Failed to identify benchmark sample: %s\n  %s', benchmark,
+                  target_sample_id)
+  return None
+
+
 def truncate_logs(logs: str, max_len: int) -> str:
   if len(logs) <= max_len:
     return logs
@@ -411,7 +430,7 @@ def sample_page(benchmark, sample):
   if _is_valid_benchmark_dir(benchmark):
     return render_template('sample.html',
                            benchmark=benchmark,
-                           sample=sample,
+                           sample=match_sample(benchmark, sample),
                            logs=get_logs(benchmark, sample),
                            run_logs=get_run_logs(benchmark, sample),
                            targets=get_targets(benchmark, sample),
