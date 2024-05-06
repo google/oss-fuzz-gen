3a8da5df6e905dc257cc6d0a54060666c596f41e
fdt622 <malkiya622@gmail.com>
2024-05-06 16:07:38 +0800

    change regex for crash_info

--- experiment/fuzz_target_error.py
+++ experiment/fuzz_target_error.py
@@ -45,7 +45,7 @@ class SemanticCheckResult:
   SYMPTOM_SCARINESS = re.compile(r'SCARINESS:\s*\d+\s*\((.*)\)\n')
 
   # Regex for extract crash information.
-  INFO_CRASH = re.compile(r'={65}\n(.*)', re.DOTALL)
+  INFO_CRASH = re.compile(r'^(={65})\n([\s\S]*)', re.MULTILINE | re.DOTALL)
 
   NO_COV_INCREASE_MSG_PREFIX = 'No code coverage increasement'
 
