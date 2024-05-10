7dbfc8249f0a51f77a8935149ccb7fe983c434f4
trashvisor <64044176+trashvisor@users.noreply.github.com>
2024-05-10 13:45:00 +1000

    Use introspector to retrieve context. (#144)
    Port context retrieval to use introspector APIs (moving from ASTs to
    DWARF debug information).
    
    ---------
    
    Co-authored-by: Dongge Liu <donggeliu@google.com>
    Co-authored-by: Oliver Chang <ochang@google.com>

--- llm_toolkit/prompt_builder.py
+++ llm_toolkit/prompt_builder.py
@@ -20,6 +20,8 @@ import os
 from abc import abstractmethod
 from typing import Optional, Tuple
 
+import jinja2
+
 from data_prep import project_targets
 from experiment.benchmark import Benchmark, FileType
 from experiment.fuzz_target_error import SemanticCheckResult
@@ -53,14 +55,12 @@ class PromptBuilder:
     self._prompt = model.prompt_type()()
 
   @abstractmethod
-  def build(
-      self,
-      function_signature: str,
-      target_file_type: FileType,
-      example_pair: list[list[str]],
-      project_example_content: Optional[list[list[str]]] = None,
-      project_context_content: Optional[Tuple[str,
-                                              str]] = None) -> prompts.Prompt:
+  def build(self,
+            function_signature: str,
+            target_file_type: FileType,
+            example_pair: list[list[str]],
+            project_example_content: Optional[list[list[str]]] = None,
+            project_context_content: Optional[dict] = None) -> prompts.Prompt:
     """Builds a prompt."""
 
   @abstractmethod
@@ -132,11 +132,16 @@ class DefaultTemplateBuilder(PromptBuilder):
     solution = solution.replace('{SOLUTION_CONTENT}', solution_content)
     return solution
 
-  def format_context(self, header_content: str, type_content: str) -> str:
-    context = self._get_template(self.context_template_file)
-    context = context.replace('{CONTEXT_HEADER}', header_content)
-    context = context.replace('{CONTEXT_TYPES}', type_content)
-    return context
+  def format_context(self, context_info: dict) -> str:
+    context = jinja2.Template(self._get_template(self.context_template_file),
+                              trim_blocks=True,
+                              lstrip_blocks=True)
+    return context.render(
+        headers='\n'.join(context_info['files']),
+        must_insert=context_info['decl'],
+        func_source=context_info['func_source'],
+        xrefs='\n'.join(context_info['xrefs']),
+    )
 
   def _select_examples(self, examples: list[list],
                        prompt_size: int) -> list[list[str]]:
@@ -217,26 +222,20 @@ class DefaultTemplateBuilder(PromptBuilder):
       self._prompt.add_problem(problem)
       self._prompt.add_solution(solution)
 
-  def build(
-      self,
-      function_signature: str,
-      target_file_type: FileType,
-      example_pair: list[list[str]],
-      project_example_content: Optional[list[list[str]]] = None,
-      project_context_content: Optional[Tuple[str,
-                                              str]] = None) -> prompts.Prompt:
+  def build(self,
+            function_signature: str,
+            target_file_type: FileType,
+            example_pair: list[list[str]],
+            project_example_content: Optional[list[list[str]]] = None,
+            project_context_content: Optional[dict] = None) -> prompts.Prompt:
     """Constructs a prompt using the templates in |self| and saves it."""
     priming = self._format_priming(target_file_type)
     final_problem = self.format_problem(function_signature)
     final_problem += (f'You MUST call <code>\n'
                       f'{function_signature}\n'
                       f'</code> in your solution!\n')
-    # TODO(ggryan@): Add a function to ensure the header is consistent
-    # with others (e.g., use "" or <>, use the same path prefix
-    # with other non-builtin include statements or the original fuzz target.)
     if project_context_content:
-      final_problem += self.format_context(project_context_content[0],
-                                           project_context_content[1])
+      final_problem += self.format_context(project_context_content)
     final_problem += '\n<solution>'
     self._prepare_prompt(priming, final_problem, example_pair,
                          project_example_content)
