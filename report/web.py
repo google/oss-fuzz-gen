5a8228e7ea8c232621d139542b95efcfb1353be3
Erfan Norozi <erfanio@google.com>
2024-06-10 06:21:18 +0000

    Refactor functions related to results into a class to prepare for more refactors.
    This is the first step towards refactoring the filesystem I/O into its
    own class so we can switch between local filesystem and GCS for I/O.

--- report/web.py
+++ report/web.py
@@ -31,7 +31,6 @@ import run_one_experiment
 from experiment import evaluator
 from experiment.workdir import WorkDirs
 
-RESULTS_DIR = ''
 BENCHMARK_SET_DIR = 'benchmark-sets'
 BENCHMARK_DIR = ''
 
@@ -130,237 +129,229 @@ class Target:
   fixer_prompt: Optional[str] = None
 
 
-def sample_ids(target_paths: list[str]):
-  for target in target_paths:
-    yield os.path.splitext(os.path.basename(target))[0]
+class Results:
 
+  def __init__(self, results_dir='results'):
+    self._results_dir = results_dir
 
-def get_results(benchmark) -> tuple[list[evaluator.Result], list[str]]:
-  """
-  Returns results of all samples. Items can be None if they're not complete.
-  """
-  targets = get_generated_targets(benchmark)
+  def list_benchmark_ids(self):
+    return sorted(
+        filter(self._is_valid_benchmark_dir, os.listdir(self._results_dir)))
 
-  results = []
-  status_dir = os.path.join(RESULTS_DIR, benchmark, 'status')
+  def list_benchmarks(self) -> List[Benchmark]:
+    """Lists benchmarks in the result directory."""
+    benchmarks = []
+    for benchmark in self.list_benchmark_ids():
+      results, targets = self._get_results(benchmark)
+      status = 'Done' if all(r for r in results) and results else 'Running'
 
-  for sample_id in sample_ids(targets):
-    results_path = os.path.join(status_dir, sample_id, 'result.json')
-    if not os.path.exists(results_path):
-      results.append(None)
-      continue
+      filtered_results = []
+      for i, stat in enumerate(results):
+        if stat:
+          filtered_results.append((i, stat))
 
-    with open(results_path) as f:
-      try:
-        data = json.load(f)
-      except Exception:
-        return [], []
+      if filtered_results:
+        result = run_one_experiment.aggregate_results(filtered_results, targets)
+      else:
+        result = run_one_experiment.AggregatedResult()
 
-    results.append(evaluator.Result(**data))
+      benchmarks.append(Benchmark(benchmark, status, result))
 
-  return results, targets
+    return benchmarks
 
-
-def get_prompt(benchmark) -> Optional[str]:
-  root_dir = os.path.join(RESULTS_DIR, benchmark)
-  for name in os.listdir(root_dir):
-    if re.match(r'^prompt.*txt$', name):
-      with open(os.path.join(root_dir, name)) as f:
-        return f.read()
-
-  return None
-
-
-def get_generated_targets(benchmark: str) -> list[str]:
-  targets = []
-  raw_targets_dir = os.path.join(RESULTS_DIR, benchmark, 'raw_targets')
-  for filename in sorted(os.listdir(raw_targets_dir)):
-    if os.path.splitext(filename)[1] in TARGET_EXTS:
-      targets.append(os.path.join(raw_targets_dir, filename))
-
-  return targets
-
-
-def _is_valid_benchmark_dir(cur_dir: str) -> bool:
-  """Checks if |cur_dir| is a valid benchmark directory (e.g., no lost+found)"""
-  # Check prefix.
-  if not cur_dir.startswith('output-'):
-    return False
-  # Check sub-directories.
-  expected_dirs = ['raw_targets', 'status', 'fixed_targets']
-  return all(
-      os.path.isdir(os.path.join(RESULTS_DIR, cur_dir, expected_dir))
-      for expected_dir in expected_dirs)
-
-
-def list_benchmarks() -> List[Benchmark]:
-  """Lists benchmarks in the result directory."""
-  benchmarks = []
-  benchmark_names = sorted(os.listdir(RESULTS_DIR))
-  # Not sure why there is a `lost+found` dir in |RESULTS_DIR|, which caused
-  # failures in get_generated_targets().
-  # Maybe it is from mounting?
-  # TODO(erfan): Check if not mounting to the root dir can solve this.
-  for benchmark in benchmark_names:
-    if not _is_valid_benchmark_dir(benchmark):
-      continue
-    results, targets = get_results(benchmark)
-    status = 'Done' if all(r for r in results) and results else 'Running'
-
-    filtered_results = []
-    for i, stat in enumerate(results):
-      if stat:
-        filtered_results.append((i, stat))
+  def match_benchmark(self, benchmark_id: str) -> Benchmark:
+    """Returns a benchmark class based on |benchmark_id|."""
+    results, targets = self._get_results(benchmark_id)
+    status = 'Done' if results and all(results) else 'Running'
+    filtered_results = [(i, stat) for i, stat in enumerate(results) if stat]
 
     if filtered_results:
       result = run_one_experiment.aggregate_results(filtered_results, targets)
     else:
       result = run_one_experiment.AggregatedResult()
 
-    benchmarks.append(Benchmark(benchmark, status, result))
+    return Benchmark(benchmark_id, status, result)
 
-  return benchmarks
+  def get_final_target_code(self, benchmark: str, sample: str) -> str:
+    """Gets the targets of benchmark |benchmark| with sample ID |sample|."""
+    targets_dir = os.path.join(self._results_dir, benchmark, 'fixed_targets')
 
-
-def match_benchmark(benchmark_id: str) -> Benchmark:
-  """Returns a benchmark class based on |benchmark_id|."""
-  results, targets = get_results(benchmark_id)
-  status = 'Done' if results and all(results) else 'Running'
-  filtered_results = [(i, stat) for i, stat in enumerate(results) if stat]
-
-  if filtered_results:
-    result = run_one_experiment.aggregate_results(filtered_results, targets)
-  else:
-    result = run_one_experiment.AggregatedResult()
-
-  return Benchmark(benchmark_id, status, result)
-
-
-def get_samples(benchmark: str) -> list[Sample]:
-  """Gets the samples and their status of the given benchmark |bnmk|."""
-  samples = []
-  results, _ = get_results(benchmark)
-
-  for i, sample_id in enumerate(sample_ids(get_generated_targets(benchmark))):
-    status = 'Running'
-    result = evaluator.Result()
-    if results[i]:
-      status = 'Done'
-      result = results[i]
-
-    samples.append(Sample(sample_id, status, result))
-
-  return samples
-
-
-def match_sample(benchmark: str, target_sample_id: str) -> Optional[Sample]:
-  """Identifies the samples object and its status of the given sample id."""
-  results, _ = get_results(benchmark)
-
-  for i, sample_id in enumerate(sample_ids(get_generated_targets(benchmark))):
-    if sample_id != target_sample_id:
-      continue
-    status = 'Running'
-    result = evaluator.Result()
-    if results[i]:
-      status = 'Done'
-      result = results[i]
-
-    return Sample(sample_id, status, result)
-  logging.warning('Failed to identify benchmark sample: %s\n  %s', benchmark,
-                  target_sample_id)
-  return None
-
-
-def truncate_logs(logs: str, max_len: int) -> str:
-  if len(logs) <= max_len:
-    return logs
-
-  return logs[:max_len // 2] + '\n...truncated...\n' + logs[-(max_len // 2) +
-                                                            1:]
-
-
-def get_logs(benchmark: str, sample: str) -> str:
-  status_dir = os.path.join(RESULTS_DIR, benchmark, 'status')
-  results_path = os.path.join(status_dir, sample, 'log.txt')
-  if not os.path.exists(results_path):
+    for name in sorted(os.listdir(targets_dir)):
+      path = os.path.join(targets_dir, name)
+      if os.path.isfile(path) and name.startswith(sample + '.'):
+        with open(path) as f:
+          code = f.read()
+          code = json.dumps(code)
+        return code
     return ''
 
-  with open(results_path) as f:
-    return f.read()
+  def match_sample(self, benchmark: str,
+                   target_sample_id: str) -> Optional[Sample]:
+    """Identifies the samples object and its status of the given sample id."""
+    results, _ = self._get_results(benchmark)
 
+    for i, sample_id in enumerate(
+        self._sample_ids(self._get_generated_targets(benchmark))):
+      if sample_id != target_sample_id:
+        continue
+      status = 'Running'
+      result = evaluator.Result()
+      if results[i]:
+        status = 'Done'
+        result = results[i]
 
-def get_run_logs(benchmark: str, sample: str) -> str:
-  """Returns the content of the last run log."""
-  run_logs_dir = os.path.join(RESULTS_DIR, benchmark, 'logs', 'run')
-  largest_iteration, last_log_file = -1, None
-  for name in os.listdir(run_logs_dir):
-    if name.startswith(sample + '.'):
-      iteration = WorkDirs.get_run_log_iteration(name)
-      if iteration is None:
-        # Be compatible with older results where no '-Fxx' in run log file name
-        last_log_file = name
-        break
+      return Sample(sample_id, status, result)
+    logging.warning('Failed to identify benchmark sample: %s\n  %s', benchmark,
+                    target_sample_id)
+    return None
 
-      if largest_iteration < iteration:
-        largest_iteration, last_log_file = iteration, name
+  def get_logs(self, benchmark: str, sample: str) -> str:
+    status_dir = os.path.join(self._results_dir, benchmark, 'status')
+    results_path = os.path.join(status_dir, sample, 'log.txt')
+    if not os.path.exists(results_path):
+      return ''
 
-  if not last_log_file:
+    with open(results_path) as f:
+      return f.read()
+
+  def get_run_logs(self, benchmark: str, sample: str) -> str:
+    """Returns the content of the last run log."""
+    run_logs_dir = os.path.join(self._results_dir, benchmark, 'logs', 'run')
+    largest_iteration, last_log_file = -1, None
+    for name in os.listdir(run_logs_dir):
+      if name.startswith(sample + '.'):
+        iteration = WorkDirs.get_run_log_iteration(name)
+        if iteration is None:
+          # Be compatible with older results where no '-Fxx' in run log file name
+          last_log_file = name
+          break
+
+        if largest_iteration < iteration:
+          largest_iteration, last_log_file = iteration, name
+
+    if not last_log_file:
+      return ''
+
+    with open(os.path.join(run_logs_dir, last_log_file), errors='replace') as f:
+      return self._truncate_logs(f.read(), MAX_RUN_LOGS_LEN)
+
     return ''
 
-  with open(os.path.join(run_logs_dir, last_log_file), errors='replace') as f:
-    return truncate_logs(f.read(), MAX_RUN_LOGS_LEN)
+  def get_targets(self, benchmark: str, sample: str) -> list[Target]:
+    """Gets the targets of benchmark |benchmark| with sample ID |sample|."""
+    targets_dir = os.path.join(self._results_dir, benchmark, 'fixed_targets')
+    targets = []
 
-  return ''
+    for name in sorted(os.listdir(targets_dir)):
+      path = os.path.join(targets_dir, name)
+      if os.path.isfile(path) and name.startswith(sample + '.'):
+        logging.debug(path)
+        with open(path) as f:
+          code = f.read()
+        targets.insert(0, Target(code=code))
 
+      if os.path.isdir(path) and name.startswith(sample + '-F'):
+        targets.append(self._get_fixed_target(path))
 
-def get_fixed_target(path):
-  """Gets the fixed fuzz target from the benchmark's result |path|."""
-  code = ''
-  fixer_prompt = ''
-  for name in os.listdir(path):
-    if name.endswith('.txt'):
-      with open(os.path.join(path, name)) as f:
-        fixer_prompt = f.read()
+    return targets
 
-    if name.endswith('.rawoutput'):
-      with open(os.path.join(path, name)) as f:
-        code = f.read()
+  def get_samples(self, benchmark: str) -> list[Sample]:
+    """Gets the samples and their status of the given benchmark |bnmk|."""
+    samples = []
+    results, _ = self._get_results(benchmark)
 
-  return Target(code, fixer_prompt)
+    for i, sample_id in enumerate(
+        self._sample_ids(self._get_generated_targets(benchmark))):
+      status = 'Running'
+      result = evaluator.Result()
+      if results[i]:
+        status = 'Done'
+        result = results[i]
 
+      samples.append(Sample(sample_id, status, result))
 
-def get_targets(benchmark: str, sample: str) -> list[Target]:
-  """Gets the targets of benchmark |benchmark| with sample ID |sample|."""
-  targets_dir = os.path.join(RESULTS_DIR, benchmark, 'fixed_targets')
-  targets = []
+    return samples
 
-  for name in sorted(os.listdir(targets_dir)):
-    path = os.path.join(targets_dir, name)
-    if os.path.isfile(path) and name.startswith(sample + '.'):
-      logging.debug(path)
-      with open(path) as f:
-        code = f.read()
-      targets.insert(0, Target(code=code))
+  def get_prompt(self, benchmark: str) -> Optional[str]:
+    root_dir = os.path.join(self._results_dir, benchmark)
+    for name in os.listdir(root_dir):
+      if re.match(r'^prompt.*txt$', name):
+        with open(os.path.join(root_dir, name)) as f:
+          return f.read()
 
-    if os.path.isdir(path) and name.startswith(sample + '-F'):
-      targets.append(get_fixed_target(path))
+    return None
 
-  return targets
+  def _get_results(self,
+                   benchmark: str) -> tuple[list[evaluator.Result], list[str]]:
+    """
+    Returns results of all samples. Items can be None if they're not complete.
+    """
+    targets = self._get_generated_targets(benchmark)
 
+    results = []
+    status_dir = os.path.join(self._results_dir, benchmark, 'status')
 
-def get_final_target_code(benchmark: str, sample: str) -> str:
-  """Gets the targets of benchmark |benchmark| with sample ID |sample|."""
-  targets_dir = os.path.join(RESULTS_DIR, benchmark, 'fixed_targets')
+    for sample_id in self._sample_ids(targets):
+      results_path = os.path.join(status_dir, sample_id, 'result.json')
+      if not os.path.exists(results_path):
+        results.append(None)
+        continue
 
-  for name in sorted(os.listdir(targets_dir)):
-    path = os.path.join(targets_dir, name)
-    if os.path.isfile(path) and name.startswith(sample + '.'):
-      with open(path) as f:
-        code = f.read()
-        code = json.dumps(code)
-      return code
-  return ''
+      with open(results_path) as f:
+        try:
+          data = json.load(f)
+        except Exception:
+          return [], []
+
+      results.append(evaluator.Result(**data))
+
+    return results, targets
+
+  def _is_valid_benchmark_dir(self, cur_dir: str) -> bool:
+    """Checks if |cur_dir| is a valid benchmark directory (e.g., no lost+found)"""
+    # Check prefix.
+    if not cur_dir.startswith('output-'):
+      return False
+    # Check sub-directories.
+    expected_dirs = ['raw_targets', 'status', 'fixed_targets']
+    return all(
+        os.path.isdir(os.path.join(self._results_dir, cur_dir, expected_dir))
+        for expected_dir in expected_dirs)
+
+  def _get_generated_targets(self, benchmark: str) -> list[str]:
+    targets = []
+    raw_targets_dir = os.path.join(self._results_dir, benchmark, 'raw_targets')
+    for filename in sorted(os.listdir(raw_targets_dir)):
+      if os.path.splitext(filename)[1] in TARGET_EXTS:
+        targets.append(os.path.join(raw_targets_dir, filename))
+
+    return targets
+
+  def _get_fixed_target(self, path: str):
+    """Gets the fixed fuzz target from the benchmark's result |path|."""
+    code = ''
+    fixer_prompt = ''
+    for name in os.listdir(path):
+      if name.endswith('.txt'):
+        with open(os.path.join(path, name)) as f:
+          fixer_prompt = f.read()
+
+      if name.endswith('.rawoutput'):
+        with open(os.path.join(path, name)) as f:
+          code = f.read()
+
+    return Target(code, fixer_prompt)
+
+  def _sample_ids(self, target_paths: list[str]):
+    for target in target_paths:
+      yield os.path.splitext(os.path.basename(target))[0]
+
+  def _truncate_logs(self, logs: str, max_len: int) -> str:
+    if len(logs) <= max_len:
+      return logs
+
+    return logs[:max_len // 2] + '\n...truncated...\n' + logs[-(max_len // 2) +
+                                                              1:]
 
 
 class JinjaTemplate:
@@ -400,19 +391,21 @@ class JinjaTemplate:
 class GenerateReport:
 
   def __init__(self,
+               results: Results,
                output_dir: str = 'results-report',
                template_globals: Dict[str, Any] = {}):
+    self._results = results
     self._output_dir = output_dir
     self._jinja = JinjaTemplate(template_globals=template_globals)
 
   def generate(self):
     self._write_index_html()
     self._write_index_json()
-    for benchmark in list_benchmarks():
-      self._write_benchmark_index(benchmark.id)
-      self._write_benchmark_crash(benchmark.id)
-      for sample in get_samples(benchmark.id):
-        self._write_benchmark_sample(benchmark.id, sample.id)
+    for benchmark in self._results.list_benchmark_ids():
+      self._write_benchmark_index(benchmark)
+      self._write_benchmark_crash(benchmark)
+      for sample in self._results.get_samples(benchmark):
+        self._write_benchmark_sample(benchmark, sample.id)
 
   def _write(self, output_path: str, content: str):
     full_path = os.path.join(self._output_dir, output_path)
@@ -429,28 +422,31 @@ class GenerateReport:
       f.write(content)
 
   def _write_index_html(self):
-    rendered = self._jinja.render('index.html', benchmarks=list_benchmarks())
+    rendered = self._jinja.render('index.html',
+                                  benchmarks=self._results.list_benchmarks())
     self._write('index.html', rendered)
 
   def _write_index_json(self):
-    rendered = self._jinja.render('index.json', benchmarks=list_benchmarks())
+    rendered = self._jinja.render('index.json',
+                                  benchmarks=self._results.list_benchmarks())
     self._write('index.json', rendered)
 
   def _write_benchmark_index(self, benchmark_id: str):
-    rendered = self._jinja.render('benchmark.html',
-                                  benchmark=benchmark_id,
-                                  samples=get_samples(benchmark_id),
-                                  prompt=get_prompt(benchmark_id))
+    rendered = self._jinja.render(
+        'benchmark.html',
+        benchmark=benchmark_id,
+        samples=self._results.get_samples(benchmark_id),
+        prompt=self._results.get_prompt(benchmark_id))
     self._write(f'benchmark/{benchmark_id}/index.html', rendered)
 
   def _write_benchmark_crash(self, benchmark_id: str):
     try:
       rendered = self._jinja.render(
           'crash.json',
-          benchmark=match_benchmark(benchmark_id).signature,
-          samples=get_samples(benchmark_id),
-          get_benchmark_final_target_code=partial(get_final_target_code,
-                                                  benchmark_id))
+          benchmark=self._results.match_benchmark(benchmark_id).signature,
+          samples=self._results.get_samples(benchmark_id),
+          get_benchmark_final_target_code=partial(
+              self._results.get_final_target_code, benchmark_id))
       self._write(f'benchmark/{benchmark_id}/crash.json', rendered)
     except Exception as e:
       print(f'Failed to write benchmark/{benchmark_id}/crash.json:\n{e}')
@@ -460,10 +456,10 @@ class GenerateReport:
       rendered = self._jinja.render(
           'sample.html',
           benchmark=benchmark_id,
-          sample=match_sample(benchmark_id, sample_id),
-          logs=get_logs(benchmark_id, sample_id),
-          run_logs=get_run_logs(benchmark_id, sample_id),
-          targets=get_targets(benchmark_id, sample_id))
+          sample=self._results.match_sample(benchmark_id, sample_id),
+          logs=self._results.get_logs(benchmark_id, sample_id),
+          run_logs=self._results.get_run_logs(benchmark_id, sample_id),
+          targets=self._results.get_targets(benchmark_id, sample_id))
       self._write(f'sample/{benchmark_id}/{sample_id}', rendered)
     except Exception as e:
       print(f'Failed to write sample/{benchmark_id}/{sample_id}:\n{e}')
@@ -476,9 +472,11 @@ if __name__ == '__main__':
   model = sys.argv[3] if len(sys.argv) > 3 else ''
   output_dir = sys.argv[4] if len(sys.argv) > 4 else 'results-report'
 
-  RESULTS_DIR = results_dir
   if benchmark_set:
     BENCHMARK_DIR = os.path.join(BENCHMARK_SET_DIR, benchmark_set)
 
-  gr = GenerateReport(output_dir=output_dir, template_globals={'model': model})
+  results = Results(results_dir=results_dir)
+  gr = GenerateReport(results=results,
+                      output_dir=output_dir,
+                      template_globals={'model': model})
   gr.generate()
