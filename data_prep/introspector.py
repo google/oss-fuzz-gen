8d3cee8ba962b9432358f4cabe6d4f28d1f9b0cf
David Korczynski <david@adalogics.com>
2024-06-15 11:50:22 -0700

    introspector: add new set of endpoints
    Signed-off-by: David Korczynski <david@adalogics.com>

--- data_prep/introspector.py
+++ data_prep/introspector.py
@@ -49,6 +49,10 @@ INTROSPECTOR_XREF = ''
 INTROSPECTOR_TYPE = ''
 INTROSPECTOR_FUNC_SIG = ''
 INTROSPECTOR_ADDR_TYPE = ''
+INTROSPECTOR_ALL_HEADER_FILES = ''
+INTROSPECTOR_ALL_FUNC_TYPES = ''
+INTROSPECTOR_REPOSITORY_API = ''
+INTROSPECTOR_SAMPLE_XREFS = ''
 
 
 def get_oracle_dict() -> Dict[str, Any]:
@@ -66,7 +70,9 @@ def set_introspector_endpoints(endpoint):
   global INTROSPECTOR_ENDPOINT, INTROSPECTOR_CFG, INTROSPECTOR_FUNC_SIG, \
       INTROSPECTOR_FUNCTION_SOURCE, INTROSPECTOR_PROJECT_SOURCE, \
       INTROSPECTOR_XREF, INTROSPECTOR_TYPE, INTROSPECTOR_ORACLE_FAR_REACH, \
-      INTROSPECTOR_ORACLE_KEYWORD, INTROSPECTOR_ADDR_TYPE
+      INTROSPECTOR_ORACLE_KEYWORD, INTROSPECTOR_ADDR_TYPE, \
+      INTROSPECTOR_ALL_HEADER_FILES, INTROSPECTOR_ALL_FUNC_TYPES, \
+      INTROSPECTOR_REPOSITORY_API, INTROSPECTOR_SAMPLE_XREFS
 
   INTROSPECTOR_ENDPOINT = endpoint
   logging.info('Fuzz Introspector endpoint set to %s', INTROSPECTOR_ENDPOINT)
@@ -83,6 +89,14 @@ def set_introspector_endpoints(endpoint):
   INTROSPECTOR_FUNC_SIG = f'{INTROSPECTOR_ENDPOINT}/function-signature'
   INTROSPECTOR_ADDR_TYPE = (
       f'{INTROSPECTOR_ENDPOINT}/addr-to-recursive-dwarf-info')
+  INTROSPECTOR_ALL_HEADER_FILES = (
+      f'{INTROSPECTOR_ENDPOINT}/all-header-files')
+  INTROSPECTOR_ALL_FUNC_TYPES = (
+      f'{INTROSPECTOR_ENDPOINT}/func-debug-types')
+  INTROSPECTOR_REPOSITORY_API = (
+          f'{INTROSPECTOR_ENDPOINT}/project-repository')
+  INTROSPECTOR_SAMPLE_XREFS = (
+          f'{INTROSPECTOR_ENDPOINT}/sample-cross-references')
 
 
 def _construct_url(api: str, params: dict) -> str:
@@ -206,6 +220,33 @@ def query_introspector_source_code(project: str, filepath: str, begin_line: int,
 
   return _get_data(resp, 'source_code', '')
 
+
+def query_introspector_header_files(project: str) -> List[str]:
+  resp = _query_introspector(INTROSPECTOR_ALL_HEADER_FILES, {'project': project})
+  all_header_files = _get_data(resp, 'all-header-files', [])
+  return all_header_files
+
+def query_introspector_sample_xrefs(project: str, func_sig: str) -> List[str]:
+  """Queries FuzzIntrospector API for source code of |func_sig|."""
+  resp = _query_introspector(INTROSPECTOR_SAMPLE_XREFS, {
+      'project': project,
+      'function_signature': func_sig
+  })
+  return _get_data(resp, 'source-code-refs', [])
+
+def query_introspector_project_repository(project: str) -> str:
+  """Queries a fuzz target oracle API from Fuzz Introspector."""
+  resp = _query_introspector(INTROSPECTOR_REPOSITORY_API, {'project': project})
+  return _get_data(resp, 'project-repository', '')
+
+def query_introspector_function_debug_arg_types(project: str, func_sig: str) -> List[str]:
+  """Queries FuzzIntrospector API for source code of |func_sig|."""
+  resp = _query_introspector(INTROSPECTOR_ALL_FUNC_TYPES, {
+      'project': project,
+      'function_signature': func_sig
+  })
+  arg_types = _get_data(resp, 'arg-types', [])
+  return arg_types
 
 def query_introspector_cross_references(project: str,
                                         func_sig: str) -> list[str]:
