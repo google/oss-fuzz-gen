google/oss-fuzz-gen

commit 48b0a81920e6e3fda2856e26ccf92b309c867da2

Revive C-specific prompt and examples (#382)
Observing many compatibility issues caused by building a C++ fuzz target
with C projects, I want to test whether separating them can lead to a
higher compilation rate.

This PR experiment uses C-specific prompts and examples for C projects.
It also uses the fix in https://github.com/google/oss-fuzz/pull/12116

data_prep/project_context/context_introspector.py
@@ -18,12 +18,12 @@ class ContextRetriever:
     self._benchmark = benchmark
 
   def _get_embeddable_declaration(self) -> str:
-    """Retrieves declaration by language. Attach extern C to C projects."""
+    """Retrieves declaration by language."""
     lang = self._benchmark.language.lower()
     sig = self._benchmark.function_signature + ';'
 
     if lang == 'c':
-      return 'extern "C" ' + sig
+      return sig
 
     if lang != 'c++':
       logging.warning('Unsupported decl - Lang: %s Project: %s', lang,

llm_toolkit/prompt_builder.py
@@ -40,6 +40,10 @@ FDP_EXAMPLE_1_PROBLEM = os.path.join(EXAMPLE_PATH, 'gdImageString-problem.txt')
 FDP_EXAMPLE_1_SOLUTION = os.path.join(EXAMPLE_PATH, 'gdImageString-solution.cc')
 FDP_EXAMPLE_2_PROBLEM = os.path.join(EXAMPLE_PATH, 'mpg123_decode-problem.txt')
 FDP_EXAMPLE_2_SOLUTION = os.path.join(EXAMPLE_PATH, 'mpg123_decode-solution.cc')
+C_EXAMPLE_1_PROBLEM = os.path.join(EXAMPLE_PATH, 'fuzzerPolygonToCells.txt')
+C_EXAMPLE_1_SOLUTION = os.path.join(EXAMPLE_PATH, 'fuzzerPolygonToCells.c')
+C_EXAMPLE_2_PROBLEM = os.path.join(EXAMPLE_PATH, 'dns_message_parse.txt')
+C_EXAMPLE_2_SOLUTION = os.path.join(EXAMPLE_PATH, 'dns_message_parse.c')
 FDP_JVM_EXAMPLE_1_PROBLEM = os.path.join(EXAMPLE_PATH, 'joni_regex-problem.txt')
 FDP_JVM_EXAMPLE_1_SOLUTION = os.path.join(EXAMPLE_PATH,
                                           'joni_regex-solution.java')
@@ -54,8 +58,8 @@ EXAMPLES = {
         [FDP_EXAMPLE_2_PROBLEM, FDP_EXAMPLE_2_SOLUTION],
     ],
     'c': [
-        [FDP_EXAMPLE_1_PROBLEM, FDP_EXAMPLE_1_SOLUTION],
-        [FDP_EXAMPLE_2_PROBLEM, FDP_EXAMPLE_2_SOLUTION],
+        [C_EXAMPLE_1_PROBLEM, C_EXAMPLE_1_SOLUTION],
+        [C_EXAMPLE_2_PROBLEM, C_EXAMPLE_2_SOLUTION],
     ],
     'jvm': [
         [FDP_JVM_EXAMPLE_1_PROBLEM, FDP_JVM_EXAMPLE_1_SOLUTION],
@@ -139,7 +143,8 @@ class DefaultTemplateBuilder(PromptBuilder):
   def _format_priming(self, target_file_type: FileType) -> str:
     """Formats a priming based on the prompt template."""
     priming = self._get_template(self.priming_template_file)
-    if target_file_type in [FileType.C, FileType.CPP]:
+    priming = priming.replace('{LANGUAGE}', target_file_type.value)
+    if target_file_type == FileType.CPP:
       type_specific_priming = self._get_template(self.cpp_priming_filler_file)
     else:
       type_specific_priming = ''
@@ -286,17 +291,18 @@ class DefaultTemplateBuilder(PromptBuilder):
                          error_desc: Optional[str],
                          errors: list[str]) -> prompts.Prompt:
     """Prepares the code-fixing prompt."""
-    priming, priming_weight = self._format_fixer_priming()
+    priming, priming_weight = self._format_fixer_priming(benchmark)
     problem = self._format_fixer_problem(raw_code, error_desc, errors,
                                          priming_weight)
 
     self._prepare_prompt(priming, problem)
     return self._prompt
 
-  def _format_fixer_priming(self) -> Tuple[str, int]:
+  def _format_fixer_priming(self, benchmark: Benchmark) -> Tuple[str, int]:
     """Formats a priming for code fixer based on the template."""
     with open(self.fixer_priming_template_file) as f:
       priming = f.read().strip() + '\n'
+    priming = priming.replace('{LANGUAGE}', benchmark.language)
     priming_prompt = self._prompt.create_prompt_piece(priming, 'system')
     priming_weight = self._model.estimate_token_num(priming_prompt)
     # NOTE: We need to return the priming _as text_ and the weight. Otherwise,

prompts/example/dns_message_parse.c
@@ -1,0 +1,174 @@
+/*
+ * Copyright (C) Internet Systems Consortium, Inc. ("ISC")
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, you can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * See the COPYRIGHT file distributed with this work for additional
+ * information regarding copyright ownership.
+ */
+
+#include <inttypes.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+#include <isc/buffer.h>
+#include <isc/commandline.h>
+#include <isc/file.h>
+#include <isc/mem.h>
+#include <isc/result.h>
+#include <isc/string.h>
+#include <isc/util.h>
+
+#include <dns/message.h>
+
+#include "fuzz.h"
+
+bool debug = false;
+
+static isc_mem_t *mctx = NULL;
+static uint8_t *output = NULL;
+static size_t output_len = 1024;
+static uint8_t render_buf[64 * 1024 - 1];
+
+int LLVMFuzzerInitialize(int *argc ISC_ATTR_UNUSED, char ***argv ISC_ATTR_UNUSED) {
+  isc_mem_create(&mctx);
+  output = isc_mem_get(mctx, output_len);
+
+  return (0);
+}
+
+static isc_result_t parse_message(isc_buffer_t *input, dns_message_t **messagep) {
+  isc_result_t result;
+  dns_message_t *message = NULL;
+
+  dns_message_create(mctx, NULL, NULL, DNS_MESSAGE_INTENTPARSE, &message);
+
+  result = dns_message_parse(message, input, DNS_MESSAGEPARSE_BESTEFFORT);
+  if (result == DNS_R_RECOVERABLE) {
+    result = ISC_R_SUCCESS;
+  }
+
+  if (result == ISC_R_SUCCESS && messagep != NULL) {
+    *messagep = message;
+  } else {
+    dns_message_detach(&message);
+  }
+
+  return (result);
+}
+
+static isc_result_t print_message(dns_message_t *message) {
+  isc_result_t result;
+  isc_buffer_t buffer;
+
+  do {
+    isc_buffer_init(&buffer, output, output_len);
+    result = dns_message_totext(message, &dns_master_style_debug, 0, &buffer);
+    if (result == ISC_R_NOSPACE) {
+      isc_mem_put(mctx, output, output_len);
+      output_len *= 2;
+      output = isc_mem_get(mctx, output_len);
+      continue;
+    }
+  } while (result == ISC_R_NOSPACE);
+
+  if (debug) {
+    fprintf(stderr, "%.*s\n", (int)isc_buffer_usedlength(&buffer), output);
+  }
+
+  return (result);
+}
+
+#define CHECKRESULT(r, f)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              \
+  {                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    \
+    r = (f);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           \
+    if (r != ISC_R_SUCCESS) {                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          \
+      goto cleanup;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    \
+    }                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  \
+  }
+
+static isc_result_t render_message(dns_message_t **messagep) {
+  isc_result_t result;
+  dns_message_t *message = *messagep;
+  isc_buffer_t buffer;
+  dns_compress_t cctx;
+
+  isc_buffer_init(&buffer, render_buf, sizeof(render_buf));
+
+  message->from_to_wire = DNS_MESSAGE_INTENTRENDER;
+  for (size_t i = 0; i < DNS_SECTION_MAX; i++) {
+    message->counts[i] = 0;
+  }
+
+  dns_compress_init(&cctx, mctx, 0);
+
+  CHECKRESULT(result, dns_message_renderbegin(message, &cctx, &buffer));
+
+  CHECKRESULT(result, dns_message_rendersection(message, DNS_SECTION_QUESTION, 0));
+
+  CHECKRESULT(result, dns_message_rendersection(message, DNS_SECTION_ANSWER, 0));
+  CHECKRESULT(result, dns_message_rendersection(message, DNS_SECTION_AUTHORITY, 0));
+
+  CHECKRESULT(result, dns_message_rendersection(message, DNS_SECTION_ADDITIONAL, 0));
+
+  dns_message_renderend(message);
+
+  dns_compress_invalidate(&cctx);
+
+  message->from_to_wire = DNS_MESSAGE_INTENTPARSE;
+
+  dns_message_detach(messagep);
+
+  result = parse_message(&buffer, messagep);
+
+  return (result);
+
+cleanup:
+  dns_compress_invalidate(&cctx);
+  return (result);
+}
+
+int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
+  isc_buffer_t buffer;
+  isc_result_t result;
+  dns_message_t *message = NULL;
+
+  if (size > 65535) {
+    return (0);
+  }
+
+  isc_buffer_constinit(&buffer, data, size);
+  isc_buffer_add(&buffer, size);
+  isc_buffer_setactive(&buffer, size);
+
+  result = parse_message(&buffer, &message);
+  if (result != ISC_R_SUCCESS) {
+    goto cleanup;
+  }
+
+  result = print_message(message);
+  if (result != ISC_R_SUCCESS) {
+    goto cleanup;
+  }
+
+  result = render_message(&message);
+  if (result != ISC_R_SUCCESS) {
+    goto cleanup;
+  }
+
+  result = print_message(message);
+  if (result != ISC_R_SUCCESS) {
+    goto cleanup;
+  }
+
+cleanup:
+  if (message != NULL) {
+    dns_message_detach(&message);
+  }
+
+  return (0);
+}

prompts/example/fuzzerPolygonToCells.c
@@ -1,0 +1,99 @@
+/*
+ * Copyright 2022 Uber Technologies, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/** @file
+ * @brief Fuzzer program for polygonToCells and related functions
+ */
+
+#include "aflHarness.h"
+#include "h3api.h"
+#include "utility.h"
+
+typedef struct {
+  int res;
+  int numHoles;
+  // repeating: num verts, verts
+  // We add a large fixed buffer so our test case generator for AFL
+  // knows how large to make the file.
+  uint8_t buffer[1024];
+} inputArgs;
+
+const int MAX_RES = 15;
+const int MAX_SZ = 4000000;
+const int MAX_HOLES = 100;
+
+int populateGeoLoop(GeoLoop *g, const uint8_t *data, size_t *offset, size_t size) {
+  if (size < *offset + sizeof(int)) {
+    return 1;
+  }
+  int numVerts = *(const int *)(data + *offset);
+  *offset = *offset + sizeof(int);
+  g->numVerts = numVerts;
+  if (size < *offset + sizeof(LatLng) * numVerts) {
+    return 1;
+  }
+  g->verts = (LatLng *)(data + *offset);
+  *offset = *offset + sizeof(LatLng) * numVerts;
+  return 0;
+}
+
+void run(GeoPolygon *geoPolygon, uint32_t flags, int res) {
+  int64_t sz;
+  H3Error err = H3_EXPORT(maxPolygonToCellsSize)(geoPolygon, res, flags, &sz);
+  if (!err && sz < MAX_SZ) {
+    H3Index *out = calloc(sz, sizeof(H3Index));
+    H3_EXPORT(polygonToCells)(geoPolygon, res, flags, out);
+    free(out);
+  }
+}
+
+int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
+  // TODO: It is difficult for the fuzzer to generate inputs that are
+  // considered valid by this fuzzer. fuzzerPolygonToCellsNoHoles.c
+  // is a workaround for that.
+  if (size < sizeof(inputArgs)) {
+    return 0;
+  }
+  const inputArgs *args = (const inputArgs *)data;
+  int res = args->res % (MAX_RES + 1);
+
+  GeoPolygon geoPolygon;
+  geoPolygon.numHoles = args->numHoles % MAX_HOLES;
+  if (geoPolygon.numHoles < 0) {
+    return 0;
+  }
+  geoPolygon.holes = calloc(geoPolygon.numHoles, sizeof(GeoLoop));
+  size_t offset = sizeof(inputArgs) - sizeof(args->buffer);
+  if (populateGeoLoop(&geoPolygon.geoloop, data, &offset, size)) {
+    free(geoPolygon.holes);
+    return 0;
+  }
+  for (int i = 0; i < geoPolygon.numHoles; i++) {
+    if (populateGeoLoop(&geoPolygon.holes[i], data, &offset, size)) {
+      free(geoPolygon.holes);
+      return 0;
+    }
+  }
+
+  // TODO: Fuzz the `flags` input as well when it has meaningful input
+  run(&geoPolygon, 0, res);
+  geoPolygon.numHoles = 0;
+  run(&geoPolygon, 0, res);
+  free(geoPolygon.holes);
+
+  return 0;
+}
+
+AFL_HARNESS_MAIN(sizeof(inputArgs));

commit 46b80996c3adfbf55835ae5d90b04d7e9aacbb0f

Add `extern` only when a c project has c++ fuzz targets (#393)
Almost all regressions in
https://github.com/google/oss-fuzz-gen/pull/382#issuecomment-2190303945
are due to C projects using C++ fuzz targets.
This PR resolves that.

data_prep/project_context/context_introspector.py
@@ -18,12 +18,12 @@ class ContextRetriever:
     self._benchmark = benchmark
 
   def _get_embeddable_declaration(self) -> str:
-    """Retrieves declaration by language."""
+    """Retrieves declaration by language.  Attach extern C if needed."""
     lang = self._benchmark.language.lower()
     sig = self._benchmark.function_signature + ';'
 
-    if lang == 'c':
-      return sig
+    if self._benchmark.needs_extern:
+      return 'extern "C" ' + sig
 
     if lang != 'c++':
       logging.warning('Unsupported decl - Lang: %s Project: %s', lang,

data_prep/project_targets.py
@@ -168,17 +168,21 @@ def generate_data(project_name: str,
 
 def _remove_header_comments(code: str) -> str:
   """Removes comments and empty lines in the code."""
-  # Remove single-line comments.
-  single_line_comment = re.compile(r'//.*?\n')
-  code = re.sub(single_line_comment, '\n', code)
-
   # Remove multi-line comments.
   multi_line_comment = re.compile(r'/\*.*?\*/', re.DOTALL)
   code = re.sub(multi_line_comment, '', code)
 
+  # Remove single-line comments.
+  single_line_comment = re.compile(r'(?:^|\s+)//.*\n')
+  code = re.sub(single_line_comment, '\n', code)
+
   # Remove empty lines.
   empty_line = re.compile(r'\n+\s*\n+')
   code = re.sub(empty_line, '\n', code)
+
+  # Trim all newlines and spaces.
+  code.lstrip('\n ')
+  code.rstrip('\n ')
   return code
 
 

experiment/benchmark.py
@@ -174,6 +174,12 @@ class Benchmark:
     """Returns the file type of the benchmark."""
     return get_file_type(self.target_path)
 
+  @property
+  def needs_extern(self) -> bool:
+    """Checks if it is C++ fuzz target for a C project, which needs `extern`."""
+    return (self.file_type.value.lower() == 'c++' and
+            self.language.lower() == 'c')
+
 
 def get_file_type(file_path: str) -> FileType:
   """Returns the file type based on the extension of |file_name|."""

llm_toolkit/prompt_builder.py
@@ -92,7 +92,8 @@ class PromptBuilder:
             target_file_type: FileType,
             example_pair: list[list[str]],
             project_example_content: Optional[list[list[str]]] = None,
-            project_context_content: Optional[dict] = None) -> prompts.Prompt:
+            project_context_content: Optional[dict] = None,
+            needs_extern: bool = False) -> prompts.Prompt:
     """Builds a prompt."""
 
   @abstractmethod
@@ -141,10 +142,15 @@ class DefaultTemplateBuilder(PromptBuilder):
     self.triager_problem_template_file = self._find_template(
         template_dir, 'triager_problem.txt')
 
-  def _format_priming(self, target_file_type: FileType) -> str:
+  def _format_priming(self, target_file_type: FileType,
+                      needs_extern: bool) -> str:
     """Formats a priming based on the prompt template."""
     priming = self._get_template(self.priming_template_file)
     priming = priming.replace('{LANGUAGE}', target_file_type.value)
+    if needs_extern:
+      priming += ('\nNote that some code may need to be wrapped with '
+                  '<code>extern "C"</code> because the project under test is '
+                  'written in C but the fuzz target is in C++.\n')
     if target_file_type == FileType.CPP:
       type_specific_priming = self._get_template(self.cpp_priming_filler_file)
     else:
@@ -274,9 +280,10 @@ class DefaultTemplateBuilder(PromptBuilder):
             target_file_type: FileType,
             example_pair: list[list[str]],
             project_example_content: Optional[list[list[str]]] = None,
-            project_context_content: Optional[dict] = None) -> prompts.Prompt:
+            project_context_content: Optional[dict] = None,
+            needs_extern: bool = False) -> prompts.Prompt:
     """Constructs a prompt using the templates in |self| and saves it."""
-    priming = self._format_priming(target_file_type)
+    priming = self._format_priming(target_file_type, needs_extern)
     final_problem = self.format_problem(function_signature)
     final_problem += (f'You MUST call <code>\n'
                       f'{function_signature}\n'
@@ -303,7 +310,11 @@ class DefaultTemplateBuilder(PromptBuilder):
     """Formats a priming for code fixer based on the template."""
     with open(self.fixer_priming_template_file) as f:
       priming = f.read().strip() + '\n'
-    priming = priming.replace('{LANGUAGE}', benchmark.language)
+    priming = priming.replace('{LANGUAGE}', benchmark.file_type.value)
+    if benchmark.needs_extern:
+      priming += ('\nNote that some code may need to be wrapped with '
+                  '<code>extern "C"</code> because the project under test is '
+                  'written in C but the fuzz target is in C++.\n')
     priming_prompt = self._prompt.create_prompt_piece(priming, 'system')
     priming_weight = self._model.estimate_token_num(priming_prompt)
     # NOTE: We need to return the priming _as text_ and the weight. Otherwise,
@@ -733,7 +744,8 @@ class DefaultJvmTemplateBuilder(PromptBuilder):
             target_file_type: FileType,
             example_pair: list[list[str]],
             project_example_content: Optional[list[list[str]]] = None,
-            project_context_content: Optional[dict] = None) -> prompts.Prompt:
+            project_context_content: Optional[dict] = None,
+            needs_extern: bool = False) -> prompts.Prompt:
     """Constructs a prompt using the templates in |self| and saves it.
        Ignore target_file_type, project_example_content
        and project_context_content parameters.
@@ -817,7 +829,8 @@ class CSpecificBuilder(PromptBuilder):
             target_file_type: FileType,
             example_pair: list[list[str]],
             project_example_content: Optional[list[list[str]]] = None,
-            project_context_content: Optional[dict] = None) -> prompts.Prompt:
+            project_context_content: Optional[dict] = None,
+            needs_extern: bool = False) -> prompts.Prompt:
     """Constructs a prompt using the templates in |self| and saves it."""
 
     with open(self.priming_template_file, 'r') as f:

run_one_experiment.py
@@ -269,7 +269,8 @@ def run(benchmark: Benchmark,
                            benchmark.file_type,
                            example_pair,
                            project_examples,
-                           project_context_content=context_info)
+                           project_context_content=context_info,
+                           needs_extern=benchmark.needs_extern)
     prompt.save(work_dirs.prompt)
 
     if dry_run: