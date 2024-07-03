8a2e996be3345f27603cebd33581641635e058fe
Arthur Chan <arthur.chan@adalogics.com>
2024-07-03 19:33:46 +0100

    JVM: Fix bug on cross reference code retrieval (#424)
    This PR fixes a bug in the wrong source code retrieval logic for
    function cross reference.
    
    Signed-off-by: Arthur Chan <arthur.chan@adalogics.com>

--- llm_toolkit/prompt_builder.py
+++ llm_toolkit/prompt_builder.py
@@ -724,10 +724,8 @@ class DefaultJvmTemplateBuilder(PromptBuilder):
 
     # Query for source code of target method callsites
     xref_source_list = []
-    for xref in introspector.query_introspector_cross_references(
+    for xref_source in introspector.query_introspector_cross_references(
         self.benchmark.project, signature):
-      xref_source = introspector.query_introspector_function_source(
-          self.benchmark.project, xref)
       if xref_source:
         xref_source_list.append(xref_source)
 
