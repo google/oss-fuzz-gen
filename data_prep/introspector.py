5771056ae2f2674f176a7d17af7eede23cbf731b
David Korczynski <david@adalogics.com>
2024-07-27 04:19:25 -0700

    nit
    Signed-off-by: David Korczynski <david@adalogics.com>

--- data_prep/introspector.py
+++ data_prep/introspector.py
@@ -662,6 +662,11 @@ def populate_benchmarks_using_introspector(project: str, language: str,
                                            limit: int,
                                            target_oracles: List[str]):
   """Populates benchmark YAML files from the data from FuzzIntrospector."""
+
+  # If there is any oracle with test-migration then only do this oracle
+  # selection, because the benchmarks will have different .yaml structure.
+  # TODO(David): clean up benchmark code to make it more flexible for varying
+  # forms of target selectors.
   for target_oracle in target_oracles:
     if 'test-migration' in target_oracle:
       harnesses, interesting = project_src.search_source(project, [], language)
