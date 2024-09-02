ede60901345d4f8357dc68810ad4d41a9b02dc86
Dongge Liu <donggeliu@google.com>
2024-09-02 15:03:36 +1000

    Add placeholders for now

--- stage/analysis_stage.py
+++ stage/analysis_stage.py
@@ -1,6 +1,10 @@
 """Analyzing the evaluation output of the fuzz target and build script."""
+from result_classes import Result
 from stage.base_stage import BaseStage
 
 
 class AnalysisStage(BaseStage):
-  pass
+
+  def execute(self, prev_stage_results: list[Result]) -> Result:
+    # A placeholder for now.
+    return prev_stage_results[-1]
--- stage/evaluation_stage.py
+++ stage/evaluation_stage.py
@@ -1,6 +1,10 @@
 """Evaluating the fuzz target and build script."""
+from result_classes import Result
 from stage.base_stage import BaseStage
 
 
 class EvalationStage(BaseStage):
-  pass
+
+  def execute(self, prev_stage_results: list[Result]) -> Result:
+    # A placeholder for now.
+    return prev_stage_results[-1]
