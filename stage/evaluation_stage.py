cec3dba91965e12379efaeca545f27307426e3c0
Dongge Liu <donggeliu@google.com>
2024-09-03 12:39:16 +1000

    More doc strings

--- stage/evaluation_stage.py
+++ stage/evaluation_stage.py
@@ -1,13 +1,15 @@
-"""A module to evaluate the fuzz target prototype.
-Use it as a usual module locally, or as script in cloud builds.
-"""
+"""The Evaluation Stage class for measuring code coverage and capture run-time
+crashes of the fuzz targets. This stage will run the fuzz target with OSS-Fuzz
+infra and report its code coverage and crashes."""
 from results import Result
 from stage.base_stage import BaseStage
 
 
 class EvalationStage(BaseStage):
-  """The module to measure the code coverage and run-time crashes performance of
-  the fuzz target."""
+  """Evaluates fuzz targets and build scripts. This stage takes a fuzz target
+  and its build script, runs them locally or on the cloud with OSS-Fuzz infra,
+  and outputs code coverage report and run-time crash information for later
+  stages to analyze and improve on. It OSS-Fuzz infra to perform these tasks."""
 
   def execute(self, result_history: list[Result]) -> Result:
     # A placeholder for now.
