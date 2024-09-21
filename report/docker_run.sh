aa2389ab6e0a36464f6c61472bfcea9671e453f3
David Korczynski <david@adalogics.com>
2024-09-21 05:00:15 -0700

    cleanup redirection
    Signed-off-by: David Korczynski <david@adalogics.com>

--- report/docker_run.sh
+++ report/docker_run.sh
@@ -161,7 +161,7 @@ $PYTHON run_all_experiments.py \
   --introspector-endpoint ${INTROSPECTOR_ENDPOINT} \
   --temperature-list "${VARY_TEMPERATURE[@]}" \
   --model "$MODEL" \
-  $AGENT_ARG >> $LOCAL_RESULTS_DIR/logs-from-run.txt 2>&1
+  $AGENT_ARG
 
 export ret_val=$?
 
