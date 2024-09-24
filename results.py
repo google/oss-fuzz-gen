2b272f6e920de62ac641aa63e4b1089ff40e50cc
Dongge Liu <donggeliu@google.com>
2024-09-24 16:10:24 +1000

    Revert "Use dataclass for result"
    This reverts commit a7c96f3b2efb7b1f0168cd0c42bee704492b38e9.

--- results.py
+++ results.py
@@ -1,5 +1,4 @@
 """The data structure of all result kinds."""
-import dataclasses
 from typing import Any, Optional
 
 from experiment import textcov
@@ -7,7 +6,6 @@ from experiment.benchmark import Benchmark
 from experiment.workdir import WorkDirs
 
 
-@dataclasses.dataclass
 class Result:
   """A benchmark generation result."""
 
@@ -33,7 +31,17 @@ class Result:
             f'({", ".join(f"{k}={v!r}" for k, v in vars(self).items())})')
 
   def to_dict(self) -> dict:
-    return dataclasses.asdict(self)
+    return {
+        'function_signature': self.benchmark.function_signature,
+        'project': self.benchmark.project,
+        'project_commit': self.benchmark.commit,
+        'project_language': self.benchmark.language,
+        'trial': self.trial,
+        'fuzz_target_source': self.fuzz_target_source,
+        'build_script_source': self.build_script_source,
+        'author': str(self.author),
+        'agent_dialogs': self.agent_dialogs,
+    }
 
 
 class BuildResult(Result):
@@ -55,6 +63,13 @@ class BuildResult(Result):
     self.compiles: bool = compiles  # Build success/failure.
     self.compile_error: str = compile_error  # Build error message.
     self.compile_log: str = compile_log  # Build full output.
+
+  def to_dict(self) -> dict:
+    return super().to_dict() | {
+        'compiles': self.compiles,
+        'compile_error': self.compile_error,
+        'compile_log': self.compile_log,
+    }
 
   @property
   def success(self):
@@ -105,6 +120,23 @@ class RunResult(BuildResult):
     self.coverage_report_path: str = coverage_report_path
     self.cov_pcs: int = cov_pcs
     self.total_pcs: int = total_pcs
+
+  def to_dict(self) -> dict:
+    return super().to_dict() | {
+        'crashes': self.crashes,
+        'run_error': self.run_error,
+        'run_log': self.run_log,
+        'coverage_summary': self.coverage_summary or {},
+        'coverage': self.coverage,
+        'line_coverage_diff': self.line_coverage_diff,
+        'reproducer_path': self.reproducer_path,
+        'textcov_diff': '',  # Class Textcov cannot be serialized.
+        'log_path': self.log_path,
+        'corpus_path': self.corpus_path,
+        'coverage_report_path': self.coverage_report_path,
+        'cov_pcs': self.cov_pcs,
+        'total_pcs': self.total_pcs,
+    }
 
   # TODO(dongge): Define success property to show if the fuzz target was run.
 
