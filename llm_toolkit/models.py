a618b4d73758bcbd1ae924acad408eac09984375
maoyixie <maoyi001@e.ntu.edu.sg>
2024-11-07 12:47:28 +0800

    fix warning

--- llm_toolkit/models.py
+++ llm_toolkit/models.py
@@ -240,7 +240,7 @@ class GPT(LLM):
 
   def get_model(self) -> str:
     """Returns the underlying model instance."""
-    self.name
+    return self.name
 
   def get_chat_client(self, model: Any) -> Any:
     """Returns a new chat session."""
