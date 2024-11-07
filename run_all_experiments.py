01c834249103bfcb000ac1f552b98f5ab12eb6cf
Dongge Liu <donggeliu@google.com>
2024-11-07 19:23:02 +1100

    Confirming the right branch

--- run_all_experiments.py
+++ run_all_experiments.py
@@ -507,7 +507,7 @@ def main():
 
   args = parse_args()
   _setup_logging(args.log_level)
-  logger.info('Starting experiments')
+  logger.info('Starting experiments on PR branch')
 
   # Capture time at start
   start = time.time()
