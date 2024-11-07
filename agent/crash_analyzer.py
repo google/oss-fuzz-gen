8d43327581d703d5ad50d386a63a1a6920dd3733
maoyixie <maoyi001@e.ntu.edu.sg>
2024-11-07 12:46:59 +0800

    add tutorial with add_problem instead of append

--- agent/crash_analyzer.py
+++ agent/crash_analyzer.py
@@ -142,10 +142,10 @@ class CrashAnalyzer(BaseAgent):
     self.analyze_tool.execute('compile > /dev/null')
     prompt = self._initial_prompt(last_result) # prompt to analyze crash
     #TODO: delete
-    print('analyzer 1st prompt:', prompt)
-    prompt.append(self.analyze_tool.tutorial())
+    print('analyzer initial prompt:', prompt)
+    prompt.add_problem(self.analyze_tool.tutorial())
     #TODO: delete
-    print('analyzer 2nd prompt:', prompt)
+    print('analyzer after append tutorial prompt:', prompt)
     crash_result = CrashResult(
       benchmark=benchmark,
       trial=last_result.trial,
