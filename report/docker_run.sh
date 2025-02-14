cde368f36a582e3f33510c5e20a2eb2d0f9af6f0
Dongge Liu <donggeliu@google.com>
2025-02-14 11:41:03 +1100

    Verify if GOOGLE APPLICATION CREDENTIALS is set

--- report/docker_run.sh
+++ report/docker_run.sh
@@ -49,6 +49,7 @@ export PYTHON
 # When running on GCP this step is unnecessary.
 if [[ $GOOGLE_APPLICATION_CREDENTIALS != '' ]]
 then
+  echo "GOOGLE APPLICATION CREDENTIALS set: ${GOOGLE_APPLICATION_CREDENTIALS}."
   gcloud auth activate-service-account LLM-EVAL@oss-fuzz.iam.gserviceaccount.com --key-file="${GOOGLE_APPLICATION_CREDENTIALS:?}"
 fi
 
