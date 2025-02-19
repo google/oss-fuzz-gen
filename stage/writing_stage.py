16bed89373744ee005bd50e4c4019b7e119df59c
DavidKorczynski <david@adalogics.com>
2025-02-19 15:08:43 +0000

    ci: add license checking (#806)
    Signed-off-by: David Korczynski <david@adalogics.com>

--- stage/writing_stage.py
+++ stage/writing_stage.py
@@ -1,3 +1,16 @@
+# Copyright 2025 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 """The Writing Stage class for generating and refining fuzz targets and their
 corresponding build scripts. This stage is responsible for creating new fuzz
 targets or improving existing ones to enhance code coverage and bug-finding
