3125657680f2115fcc760e48d988cad888d77f71
Dongge Liu <donggeliu@google.com>
2025-02-24 21:37:28 +1100

    Remove useless comments

--- agent/enhancer.py
+++ agent/enhancer.py
@@ -63,37 +63,3 @@ class Enhancer(Prototyper):
       prompt.save(self.args.work_dirs.prompt)
 
     return prompt
-
-  # def execute(self, result_history: list[Result]) -> BuildResult:
-  #   """Executes the agent based on previous result."""
-  #   last_result = result_history[-1]
-  #   logger.info('Executing Enhancer', trial=last_result.trial)
-  #   WorkDirs(self.args.work_dirs.base)
-
-  #   self.inspect_tool = ProjectContainerTool(last_result.benchmark,
-  #                                            name='inspect')
-  #   self.inspect_tool.compile(extra_commands=' && rm -rf /out/* > /dev/null')
-  #   prompt = self._initial_prompt(result_history)
-  #   build_result = BuildResult(benchmark=last_result.benchmark,
-  #                              trial=last_result.trial,
-  #                              work_dirs=last_result.work_dirs,
-  #                              author=self,
-  #                              chat_history={self.name: prompt.get()})
-  #   cur_round = 1
-  #   try:
-  #     client = self.llm.get_chat_client(model=self.llm.get_model())
-  #     while prompt and cur_round < MAX_ROUND:
-  #       response = self.chat_llm(cur_round,
-  #                                client=client,
-  #                                prompt=prompt,
-  #                                trial=last_result.trial)
-  #       prompt = self._container_tool_reaction(cur_round, response,
-  #                                              build_result)
-  #       cur_round += 1
-  #   finally:
-  #     # Cleanup: stop and remove the container
-  #     logger.debug('Stopping and removing the inspect container %s',
-  #                  self.inspect_tool.container_id,
-  #                  trial=last_result.trial)
-  #     self.inspect_tool.terminate()
-  #   return build_result
