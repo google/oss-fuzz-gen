5c2c80e8e7a6abd33cf3c11404544bcd887a211b
Dongge Liu <donggeliu@google.com>
2025-03-29 23:09:10 +1100

    temp disable caching

--- experiment/oss_fuzz_checkout.py
+++ experiment/oss_fuzz_checkout.py
@@ -32,7 +32,7 @@ logger.setLevel(logging.DEBUG)
 
 BUILD_DIR: str = 'build'
 GLOBAL_TEMP_DIR: str = ''
-ENABLE_CACHING = bool(int(os.getenv('OFG_USE_CACHING', '1')))
+ENABLE_CACHING = bool(int(os.getenv('OFG_USE_CACHING', '')))
 # Assume OSS-Fuzz is at repo root dir by default.
 # This will change if temp_dir is used.
 OSS_FUZZ_DIR: str = os.path.join(
