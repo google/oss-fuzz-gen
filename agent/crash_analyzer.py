c877dd7b0f3591a8d2b84d2c61c09152216cbdd3
maoyixie <maoyi001@e.ntu.edu.sg>
2025-03-30 16:49:09 +0800

    fix wrap lldb in bash, use screen to maintain lldb session

--- agent/crash_analyzer.py
+++ agent/crash_analyzer.py
@@ -77,7 +77,7 @@ class CrashAnalyzer(BaseAgent):
     prompt_text = ''
     for command in self._parse_tags(response, 'lldb'):
       prompt_text += self._format_lldb_execution_result(
-          tool.execute(command), previous_prompt=prompt) + '\n'
+          tool.execute_in_screen(command), previous_prompt=prompt) + '\n'
       prompt.append(prompt_text)
     return prompt
 
@@ -178,7 +178,7 @@ class CrashAnalyzer(BaseAgent):
                                  name='lldb',
                                  project_name=generated_oss_fuzz_project)
     self.analyze_tool.execute('compile > /dev/null')
-    self.analyze_tool.execute('lldb')
+    self.analyze_tool.execute("'screen -dmS lldb_session bash -c 'lldb'")
     self.check_tool = ProjectContainerTool(
         benchmark, name='check', project_name=generated_oss_fuzz_project)
     self.check_tool.compile(extra_commands=' && rm -rf /out/* > /dev/null')
