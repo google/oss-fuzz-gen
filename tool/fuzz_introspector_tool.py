google/oss-fuzz-gen

commit 72a04fa6ed2aebdfd1fee9928a655c48d91ccbb2

Integrate FunctionAnalyzer to Writing Stage (#1092)
This PR integrates the function analyzer to the pipeline's writing stage
and ensures correct data transfer from the function analyzer to other
agents.

---------

Signed-off-by: David Korczynski <david@adalogics.com>
Co-authored-by: DavidKorczynski <david@adalogics.com>
Co-authored-by: Maoyi Xie <maoyi001@e.ntu.edu.sg>
Co-authored-by: Dongge Liu <donggeliu@google.com>

tool/fuzz_introspector_tool.py
@@ -13,11 +13,21 @@
 # limitations under the License.
 """A tool for LLM agents to interact within Fuzz Introspector to access
 the project's information."""
-from tool.base_tool import BaseTool
+import logging
+
+from data_prep import introspector
+from experiment import benchmark as benchmarklib
+from tool import base_tool
+
+logger = logging.getLogger(__name__)
 
 
-class FuzzIntrospectorTool(BaseTool):
+class FuzzIntrospectorTool(base_tool.BaseTool):
   """Calls FI API with params."""
+
+  def __init__(self, benchmark: benchmarklib.Benchmark, name: str = ''):
+    super().__init__(benchmark, name)
+    self.project_functions = None
 
   def _source_code(self, filename: str, start_line: int, end_line: int) -> str:
     """Calls the source code API of the Fuzz Introspector."""
@@ -37,4 +47,87 @@ class FuzzIntrospectorTool(BaseTool):
   def _function_signature(self, function_name: str) -> str:
     """Calls the function signature API of the Fuzz Introspector."""
     # A placeholder
+    raise NotImplementedError
+
+  def function_source_with_signature(self, project_name: str,
+                                     function_signature: str) -> str:
+    """
+    Retrieves a function's source from the fuzz introspector API,
+      using the project's name and function's signature.
+
+    Args:
+        project_name (str): The name of the project.
+        function_signature (str): The signature of the function.
+
+    Returns:
+        str: Source code of the function if found, otherwise an empty string.
+    """
+
+    logger.info("Retrieving function source for '%s' in project '%s'.",
+                function_signature, project_name)
+
+    function_code = introspector.query_introspector_function_source(
+        project_name, function_signature)
+
+    if function_code.strip():
+      logger.info("Function with signature '%s' found and extracted.",
+                  function_signature)
+    else:
+      logger.error(
+          "Error: Function with signature '%s' not found in project '%s'.",
+          function_signature, project_name)
+
+    return function_code
+
+  def function_source_with_name(self, project_name: str,
+                                function_name: str) -> str:
+    """
+    Retrieves a function's source from the fuzz introspector API,
+      using the project's name and function's name.
+      This function first retrieves the list of all
+      functions in the project, so it can get the function's signature.
+      Then it uses the function's signature to retrieve the source code.
+
+    Args:
+        project_name (str): The name of the project.
+        function_name (str): The name of the function.
+
+    Returns:
+        str: Source code of the function if found, otherwise an empty string.
+    """
+
+    logger.info("Retrieving function source for '%s' in project '%s'.",
+                function_name, project_name)
+
+    if self.project_functions is None:
+      logger.info(
+          "Project functions not initialized. Initializing for project '%s'.",
+          project_name)
+      functions_list = introspector.query_introspector_all_functions(
+          project_name)
+
+      if functions_list:
+        self.project_functions = {
+            func["debug_summary"]["name"]: func
+            for func in functions_list
+            if "debug_summary" in func and "name" in func["debug_summary"]
+        }
+      else:
+        self.project_functions = None
+
+    if (self.project_functions is None or
+        function_name not in self.project_functions):
+      logger.error("Error: Required function not found for project '%s'.",
+                   project_name)
+      return ""
+
+    function_signature = self.project_functions[function_name][
+        "function_signature"]
+
+    return self.function_source_with_signature(project_name, function_signature)
+
+  def tutorial(self) -> str:
+    raise NotImplementedError
+
+  def execute(self, command: str) -> introspector.Any:
     raise NotImplementedError

agent/function_analyzer.py
@@ -17,168 +17,192 @@ The results of this analysis will be used by the writer agents to
 generate correct fuzz target for the function.
 """
 
-import logging
+import argparse
+import asyncio
+import os
+from typing import Optional
 
-from google.adk.agents import Agent
-from google.adk.runners import Runner
-from google.adk.sessions import InMemorySessionService
-from google.genai import types  # For creating message Content/Parts
+from google.adk import agents, runners, sessions
+from google.genai import types
 
-from agent.base_agent import BaseAgent
-from data_prep import introspector
+import logger
+import results as resultslib
+from agent import base_agent
 from experiment import benchmark as benchmarklib
-from llm_toolkit import prompt_builder
-from llm_toolkit.prompts import Prompt
-from results import PreWritingResult, Result
-
-logger = logging.getLogger(__name__)
+from experiment.workdir import WorkDirs
+from llm_toolkit import models, prompt_builder, prompts
+from tool import base_tool, fuzz_introspector_tool
 
 
-def get_function_source_tool(project_name: str, function_signature: str):
-  """
-  Retrieves a function's source using the project name and function signature.
-
-  Args:
-    project_name (str): The name of the project.
-    function_signature (str): The signature of the function.
-
-  Returns:
-    str: The source code of the function if found, otherwise an empty string.
-  """
-
-  function_code = introspector.query_introspector_function_source(
-      project_name, function_signature)
-
-  if function_code:
-    logger.info("Function with signature '%s' found and extracted.",
-                function_signature)
-  else:
-    logger.info(
-        "Error: Function with signature '%s' not found in project '%s'.",
-        function_signature, project_name)
-
-  return function_code
-
-
-class FunctionAnalyzer(BaseAgent):
+class FunctionAnalyzer(base_agent.BaseAgent):
   """An LLM agent to analyze a function and identify its implicit requirements.
   The results of this analysis will be used by the writer agents to
   generate correct fuzz target for the function.
   """
 
-  def initialize(self, benchmark: benchmarklib.Benchmark):
-    """Initialize the function analyzer agent with the given benchmark."""
+  def __init__(self,
+               trial: int,
+               llm: models.LLM,
+               args: argparse.Namespace,
+               benchmark: benchmarklib.Benchmark,
+               tools: Optional[list[base_tool.BaseTool]] = None,
+               name: str = ''):
 
+    # Ensure the llm is an instance of VertexAIModel
+    # TODO (pamusuo): Provide support for other LLM models
+    if not isinstance(llm, models.VertexAIModel):
+      raise ValueError(
+          "FunctionAnalyzer agent requires a VertexAIModel instance for llm.")
+
+    super().__init__(trial, llm, args, tools, name)
+
+    self.vertex_ai_model = llm._vertex_ai_model
     self.benchmark = benchmark
 
-    # Initialize the prompt builder
-    self.prompt_builder = prompt_builder.FunctionAnalyzerTemplateBuilder(
-        self.llm, self.benchmark)
+    self.initialize()
 
-    # Get the agent's instructions
-    analyzer_instruction = self.prompt_builder.build_instruction()
+  def initialize(self):
+    """Initialize the function analyzer agent with the given benchmark."""
+
+    # Initialize the Fuzz Introspector tool
+    introspector_tool = fuzz_introspector_tool.FuzzIntrospectorTool(
+        self.benchmark, self.name)
 
     # Create the agent using the ADK library
-    function_analyzer = Agent(
-        name=self.name,
-        # TODO: Get the model name from args.
-        # Currently, the default names are incompatible with the ADK library.
-        model='gemini-2.0-flash',
-        description=(
-            "Agent to analyze a function and identify its requirements."),
-        instruction=analyzer_instruction.get(),
-        tools=[get_function_source_tool])
-
-    # Get user id and session id
-    # TODO: Figure out how to get this data
-    user_id = "user"
-    session_id = "session"
+    # TODO(pamusuo): Create another AdkBaseAgent that extends
+    # BaseAgent and initializes an ADK agent as well.
+    function_analyzer = agents.LlmAgent(
+        name="FunctionAnalyzer",
+        model=self.vertex_ai_model,
+        description="""Extracts a function's requirements
+                        from its source implementation.""",
+        instruction=
+        """You are a security engineer tasked with analyzing a function
+        and extracting its input requirements,
+        necessary for it to execute correctly.""",
+        tools=[introspector_tool.function_source_with_name],
+    )
 
     # Create the session service
-    session_service = InMemorySessionService()
+    session_service = sessions.InMemorySessionService()
     session_service.create_session(
         app_name=self.name,
-        user_id=user_id,
-        session_id=session_id,
+        user_id=self.benchmark.id,
+        session_id=f"session_{self.trial}",
     )
 
     # Create the runner
-    self.runner = Runner(
+    self.runner = runners.Runner(
         agent=function_analyzer,
         app_name=self.name,
         session_service=session_service,
     )
 
-    logger.info(
-        "Function Analyzer Agent created, with name: %s, and session id: %s",
-        self.name, session_id)
+    logger.info("Function Analyzer Agent created, with name: %s",
+                self.name,
+                trial=self.trial)
 
-  def call_agent(self, query: str, runner: Runner, user_id: str,
-                 session_id: str) -> PreWritingResult:
+  async def call_agent(self, query: str, runner: runners.Runner, user_id: str,
+                       session_id: str) -> str:
     """Call the agent asynchronously with the given query."""
-
-    logger.info(">>> User query: %s", query)
 
     content = types.Content(role='user', parts=[types.Part(text=query)])
 
-    final_response_text = "Agent did not produce a final response."
+    final_response_text = ''
 
     result_available = False
 
-    for event in runner.run(
+    async for event in runner.run_async(
         user_id=user_id,
         session_id=session_id,
         new_message=content,
     ):
+
       if event.is_final_response():
-        if event.content and event.content.parts:
+        if (event.content and event.content.parts and
+            event.content.parts[0].text):
           final_response_text = event.content.parts[0].text
           result_available = True
         elif event.actions and event.actions.escalate:
-          error_message = event.error_message or 'No specific message.'
-          final_response_text = f"Agent escalated: {error_message}"
-        break
+          error_message = event.error_message
+          logger.error("Agent escalated: %s", error_message, trial=self.trial)
 
-    logger.info("<<< Agent response: %s", final_response_text)
+    logger.info("<<< Agent response: %s", final_response_text, trial=self.trial)
 
-    if result_available and final_response_text:
+    if result_available and self._parse_tag(final_response_text, 'response'):
       # Get the requirements from the response
-      requirements = self._parse_tags(final_response_text, 'requirement')
+      result_str = self._parse_tag(final_response_text, 'response')
     else:
-      requirements = []
+      result_str = ''
 
-    # Prepare the result
-    result = PreWritingResult(
+    return result_str
+
+  def write_requirements_to_file(self, args, requirements: str) -> str:
+    """Write the requirements to a file."""
+    if not requirements:
+      logger.warning("No requirements to write to file.", trial=self.trial)
+      return ''
+
+    requirement_path = os.path.join(args.work_dirs.requirements,
+                                    f"{self.benchmark.id}.txt")
+
+    with open(requirement_path, 'w') as f:
+      f.write(requirements)
+
+    logger.info("Requirements written to %s",
+                requirement_path,
+                trial=self.trial)
+
+    return requirement_path
+
+  def execute(self,
+              result_history: list[resultslib.Result]) -> resultslib.Result:
+    """Execute the agent with the given results."""
+
+    WorkDirs(self.args.work_dirs.base, keep=True)
+    logger.info('Executing %s', self.name, trial=self.trial)
+
+    result = resultslib.Result(
         benchmark=self.benchmark,
         trial=self.trial,
-        work_dirs=self.args.work_dir,
-        result_available=result_available,
-        requirements=requirements,
+        work_dirs=self.args.work_dirs,
     )
-
-    return result
-
-  def execute(self, result_history: list[Result]) -> PreWritingResult:
-    """Execute the agent with the given results."""
 
     # Call the agent asynchronously and return the result
     prompt = self._initial_prompt(result_history)
     query = prompt.gettext()
-    user_id = "user"
-    session_id = "session"
-    result = self.call_agent(query, self.runner, user_id, session_id)
 
-    if result.result_available:
-      # Save the result to the history
-      result_history.append(result)
+    # Validate query is not empty
+    if not query.strip():
+      logger.error(
+          "Error occurred while building initial prompt. Cannot call the agent.",
+          trial=self.trial)
+      return result
+
+    logger.info("Initial prompt created. Calling LLM...", trial=self.trial)
+
+    user_id = self.benchmark.id
+    session_id = f"session_{self.trial}"
+    result_str = asyncio.run(
+        self.call_agent(query, self.runner, user_id, session_id))
+
+    if result_str:
+      # Write the requirements to a file
+      requirement_path = self.write_requirements_to_file(self.args, result_str)
+      function_analysis = resultslib.FunctionAnalysisResult(requirement_path)
+      result.function_analysis = function_analysis
 
     return result
 
-  def _initial_prompt(self, results: list[Result]) -> Prompt:
+  def _initial_prompt(
+      self,
+      results: Optional[list[resultslib.Result]] = None) -> prompts.Prompt:
     """Create the initial prompt for the agent."""
 
-    prompt = self.prompt_builder.build(
-        project_name=self.benchmark.project,
-        function_signature=self.benchmark.function_signature)
+    # Initialize the prompt builder
+    builder = prompt_builder.FunctionAnalyzerTemplateBuilder(
+        self.llm, self.benchmark)
+
+    prompt = builder.build_prompt()
 
     return prompt

agent/function_based_prototyper.py
@@ -213,7 +213,7 @@ class FunctionToolPrototyper(BaseAgent):
     """Handles getting all functions in the project."""
     logger.info('Handling list_functions_in_project: %s', args, trial=-1)
 
-    functions = introspector.query_introspector_all_functions(
+    functions = introspector.query_introspector_all_signatures(
         self.benchmark.project)
     logger.info('Functions in project: %s', functions, trial=-1)
     # Extend messages to prepare for next iteration.

agent/prototyper.py
@@ -38,7 +38,8 @@ class Prototyper(BaseAgent):
 
   def _initial_prompt(self, results: list[Result]) -> Prompt:
     """Constructs initial prompt of the agent."""
-    benchmark = results[-1].benchmark
+    last_result = results[-1]
+    benchmark = last_result.benchmark
 
     if benchmark.use_project_examples:
       project_examples = project_targets.generate_data(
@@ -54,16 +55,27 @@ class Prototyper(BaseAgent):
     else:
       context_info = {}
 
+    function_analysis = last_result.function_analysis
+    if function_analysis and os.path.isfile(
+        function_analysis.function_analysis_path):
+      with open(function_analysis.function_analysis_path, 'r') as file:
+        function_requirements = file.read()
+    else:
+      function_requirements = ''
+
     builder = prompt_builder.PrototyperTemplateBuilder(
         model=self.llm,
         benchmark=benchmark,
     )
-    prompt = builder.build(example_pair=prompt_builder.EXAMPLES.get(
-        benchmark.file_type.value.lower(), []),
-                           project_example_content=project_examples,
-                           project_context_content=context_info,
-                           tool_guides=self.inspect_tool.tutorial(),
-                           project_dir=self.inspect_tool.project_dir)
+    prompt = builder.build(
+        example_pair=prompt_builder.EXAMPLES.get(
+            benchmark.file_type.value.lower(), []),
+        project_example_content=project_examples,
+        project_context_content=context_info,
+        tool_guides=self.inspect_tool.tutorial(),
+        project_dir=self.inspect_tool.project_dir,
+        function_requirements=function_requirements,
+    )
     return prompt
 
   def _update_fuzz_target_and_build_script(self, response: str,

agent_tests/function_analyzer_test.py
@@ -15,17 +15,22 @@
 
 import argparse
 import logging
+import multiprocessing
+import os
 from typing import List
 
-from agent.function_analyzer import FunctionAnalyzer
+import run_all_experiments
+from agent import function_analyzer
 from experiment import benchmark as benchmarklib
-from experiment.benchmark import Benchmark
+from experiment import workdir
 from llm_toolkit import models
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 RESULTS_DIR = './results'
+
+NUM_ANA = int(os.getenv('LLM_NUM_ANA', '2'))
 
 
 def parse_args() -> argparse.Namespace:
@@ -36,8 +41,26 @@ def parse_args() -> argparse.Namespace:
   parser.add_argument('-y',
                       '--benchmark-yaml',
                       type=str,
-                      required=True,
                       help='A benchmark YAML file.')
+
+  parser.add_argument('-b',
+                      '--benchmarks-directory',
+                      type=str,
+                      help='A directory containing benchmark YAML files.')
+
+  parser.add_argument(
+      '-g',
+      '--generate-benchmarks',
+      help=('Generate benchmarks and use those for analysis. This is a string '
+            'of comma-separated heuristics to use when identifying benchmark '
+            'targets.'),
+      type=str)
+
+  parser.add_argument('-np',
+                      '--num-pools',
+                      type=int,
+                      default=NUM_ANA,
+                      help='Number of parallel processes to use for analysis.')
 
   parser.add_argument('-w', '--work-dir', default=RESULTS_DIR)
 
@@ -49,34 +72,107 @@ def parse_args() -> argparse.Namespace:
 
   parsed_args = parser.parse_args()
 
+  benchmark_yaml = parsed_args.benchmark_yaml
+  if benchmark_yaml:
+    assert (benchmark_yaml.endswith('.yaml') or
+            benchmark_yaml.endswith('yml')), (
+                "--benchmark-yaml needs to take an YAML file.")
+
+  bench_yml = bool(benchmark_yaml)
+  bench_dir = bool(parsed_args.benchmarks_directory)
+  bench_gen = bool(parsed_args.generate_benchmarks)
+
+  num_options = int(bench_yml) + int(bench_dir) + int(bench_gen)
+  assert num_options == 1, (
+      'One and only one of --benchmark-yaml, --benchmarks-directory and '
+      '--generate-benchmarks. --benchmark-yaml takes one benchmark YAML file, '
+      '--benchmarks-directory takes: a directory of them and '
+      '--generate-benchmarks generates them during analysis.')
+
   return parsed_args
+
+
+def analyze_benchmark(benchmark: benchmarklib.Benchmark, model: models.LLM,
+                      args: argparse.Namespace) -> bool:
+  """Analyzes the benchmark using the function analyzer."""
+
+  # Initialize the function analyzer
+  analyzer = function_analyzer.FunctionAnalyzer(trial=1,
+                                                llm=model,
+                                                args=args,
+                                                benchmark=benchmark)
+
+  # Run the function analyzer
+  try:
+    result = analyzer.execute([])
+  except Exception as e:
+    logger.error("Error during analysis for benchmark %s: %s",
+                 benchmark.function_name, e)
+    return False
+
+  return result.function_analysis is not None
 
 
 if __name__ == "__main__":
 
-  model = models.LLM.setup(ai_binary='', name='vertex_ai_gemini-1-5-chat')
+  model = models.LLM.setup(ai_binary='', name='vertex_ai_gemini-2-5-pro-chat')
 
   args = parse_args()
 
-  function_analyzer = FunctionAnalyzer(trial=1, llm=model, args=args)
+  # Initialize the working directory
+  args.work_dirs = workdir.WorkDirs(args.work_dir, create_children_dirs=False)
 
-  benchmarks: List[Benchmark] = benchmarklib.Benchmark.from_yaml(
-      args.benchmark_yaml)
+  # Initialize benchmarks
+  benchmarks: List[
+      benchmarklib.Benchmark] = run_all_experiments.prepare_experiment_targets(
+          args)
 
   if len(benchmarks) == 0:
     raise ValueError("No benchmarks found in the YAML file.")
 
-  test_benchmark = benchmarks[0]
-  logger.info("Loaded benchmark for function: %s", test_benchmark.function_name)
+  logger.info("Loaded %d benchmarks from the YAML file %s.", len(benchmarks),
+              args.benchmark_yaml)
 
-  # Initialize the function analyzer with the first benchmark
-  function_analyzer.initialize(test_benchmark)
+  # Analyze each benchmark
+  success_count = 0
 
-  # Run the function analyzer
-  result = function_analyzer.execute([])
+  if NUM_ANA == 1:
+    for benchmark in benchmarks:
+      logger.info("Loaded benchmark (%d/%d) for function: %s",
+                  benchmarks.index(benchmark) + 1, len(benchmarks),
+                  benchmark.function_name)
+      if analyze_benchmark(benchmark, model, args):
+        success_count += 1
+  else:
 
-  # Print the result
-  logger.info("Function Analyzer Result:")
-  logger.info("Result available: %s", result.result_available)
-  if result.result_available:
-    logger.info("Requirements: %s", result.requirements)
+    logger.info("Running analysis in parallel with %d processes.",
+                args.num_pools)
+    with multiprocessing.Pool(args.num_pools, maxtasksperchild=1) as pool:
+
+      results = {}
+      for benchmark in benchmarks:
+        # Pass a new analyzer instance to each process to avoid sharing state
+        logger.info("Submitted benchmark (%d/%d) for function: %s to the pool.",
+                    benchmarks.index(benchmark) + 1, len(benchmarks),
+                    benchmark.function_name)
+        result = pool.apply_async(analyze_benchmark,
+                                  args=(benchmark, model, args))
+        results[benchmark.id] = result
+
+      pool.close()
+
+      # Wait for all processes to complete
+      pool.join()
+
+      # Wait for all results to complete and count successes
+      for benchmark_id, result in results.items():
+        try:
+          if result.get():
+            success_count += 1
+        except Exception as e:
+          logger.error("Error during analysis for benchmark %s: %s",
+                       benchmark_id, e)
+
+  print(
+      f"{success_count} out of {len(benchmarks)} analyses completed successfully."
+  )

benchmark-sets/quick-test/astc-encoder.yaml
@@ -1,0 +1,29 @@
+"functions":
+- "name": "_Z10encode_ise12quant_methodjPKhPhj"
+  "params":
+  - "name": "quant_level"
+    "type": "int"
+  - "name": "character_count"
+    "type": "int"
+  - "name": "input_data"
+    "type": "bool "
+  - "name": "output_data"
+    "type": "bool "
+  - "name": "bit_offset"
+    "type": "int"
+  "return_type": "void"
+  "signature": "void encode_ise(DW_TAG_enumeration_typequant_method, unsigned int, const uint8_t *, uint8_t *, unsigned int)"
+- "name": "_Z20symbolic_to_physicalRK21block_size_descriptorRK25symbolic_compressed_blockPh"
+  "params":
+  - "name": "bsd"
+    "type": "bool "
+  - "name": "scb"
+    "type": "bool "
+  - "name": "pcb"
+    "type": "bool "
+  "return_type": "void"
+  "signature": "void symbolic_to_physical(const struct block_size_descriptor &, const struct symbolic_compressed_block &, uint8_t *)"
+"language": "c++"
+"project": "astc-encoder"
+"target_name": "fuzz_astc_physical_to_symbolic"
+"target_path": "/src/astc-encoder/Source/Fuzzers/fuzz_astc_physical_to_symbolic.cpp"

benchmark-sets/quick-test/hoextdown.yaml
@@ -1,0 +1,17 @@
+"functions":
+- "name": "hoedown_document_render_inline"
+  "params":
+  - "name": "doc"
+    "type": "bool "
+  - "name": "ob"
+    "type": "bool "
+  - "name": "data"
+    "type": "bool "
+  - "name": "size"
+    "type": "size_t"
+  "return_type": "void"
+  "signature": "void hoedown_document_render_inline(hoedown_document *, hoedown_buffer *, const uint8_t *, size_t)"
+"language": "c++"
+"project": "hoextdown"
+"target_name": "hoedown_fuzzer"
+"target_path": "/src/hoextdown/test/hoedown_fuzzer.c"

benchmark-sets/quick-test/igraph.yaml
@@ -1,0 +1,21 @@
+"functions":
+- "name": "igraph_sparsemat_arpack_rssolve"
+  "params":
+  - "name": "A"
+    "type": "bool "
+  - "name": "options"
+    "type": "bool "
+  - "name": "storage"
+    "type": "bool "
+  - "name": "values"
+    "type": "bool "
+  - "name": "vectors"
+    "type": "bool "
+  - "name": "solvemethod"
+    "type": "int"
+  "return_type": "int"
+  "signature": "igraph_error_t igraph_sparsemat_arpack_rssolve(const igraph_sparsemat_t *, igraph_arpack_options_t *, igraph_arpack_storage_t *, igraph_vector_t *, igraph_matrix_t *, igraph_sparsemat_solve_t)"
+"language": "c"
+"project": "igraph"
+"target_name": "read_graphdb"
+"target_path": "/src/igraph/fuzzing/read_graphdb.cpp"

commit 1f3853c05159c8f3a7d078fb5ec2039cb34ae356

Fix bug in FI None value handling (#1118)
This PR fixes a bug introduced in #1092 where
`func["debug_summary"]["name"]` could be `None` or an empty string even
if the keys exist.

---------

Signed-off-by: Arthur Chan <arthur.chan@adalogics.com>

tool/fuzz_introspector_tool.py
@@ -107,10 +107,14 @@ class FuzzIntrospectorTool(base_tool.BaseTool):
           project_name)
 
       if functions_list:
+        # func["debug_summary"] and func["debug_summary"]["name"] could be
+        # None or empty but still exists
         self.project_functions = {
             func["debug_summary"]["name"]: func
             for func in functions_list
-            if "debug_summary" in func and "name" in func["debug_summary"]
+            if isinstance(func.get("debug_summary"), dict) and
+            isinstance(func["debug_summary"].get("name"), str) and
+            func["debug_summary"]["name"].strip()
         }
       else:
         self.project_functions = None