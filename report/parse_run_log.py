691fd572c258feebe8d08dadb09105b69c38fd45
Myan V. <myanvoos@gmail.com>
2025-07-04 11:40:22 +1200

    Add reproducer path parsing

--- report/parse_run_log.py
+++ report/parse_run_log.py
@@ -141,3 +141,14 @@ class RunLogsParser:
           }
 
     return stack_traces
+  
+  def get_crash_reproduction_path(self) -> str:
+    """Get the crash reproduction path from the run log."""
+    for line in self._lines:
+      if "Test unit written to" in line:
+        crash_match = re.search(r'Test unit written to (.+)', line)
+        if crash_match:
+          full_path = crash_match.group(1).strip()
+          filename = full_path.split('/')[-1]
+          return filename
+    return ""
